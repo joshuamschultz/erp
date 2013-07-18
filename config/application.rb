b7ec8d6a36e6994a60a3ce0f8aa81805199a55c2
Joshua Schultz <joshuamschultz@gmail.com>
2013-07-18 16:48:31 +0530

    assets added for caching

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module AllianceFasteners
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['bootstrap.js', 'app_theme.js']
   end
 end
