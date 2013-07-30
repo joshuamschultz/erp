183f5683d03f4d1071acbd14aaee5d3dd6b1f631
sreejesh <sreejeshkp@agileblaze.com>
2013-07-30 18:34:24 +0530

    gauge and dimension forms

--- app/models/gauge.rb
+++ app/models/gauge.rb
@@ -1,7 +1,13 @@
-class Gauge < ActiveRecord::Base
-  belongs_to :organization
-  has_many :dimensions
-  
+class Gauge < ActiveRecord::Base  
   attr_accessible :organization_id, :gage_caliberaion_period, :gage_caliberation_at, :gauge_active, 
   :gauge_created_id, :gauge_tool_name, :gauge_tool_no, :gauge_updated_id
+
+  after_initialize :default_values
+
+  def default_values
+	   self.gauge_active = true if self.gauge_active.nil?
+  end
+
+  belongs_to :organization
+  has_many :dimensions
 end
