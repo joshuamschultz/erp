3839da2eec58e33c93ed4d7166e83d37dfa2aad3
praveen-agileblaze <praveenm@agileblaze.com>
2013-07-31 21:21:40 +0530

    Modification in item_part_dimensions

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :owner
   has_many :item_alt_names
+  has_many :item_part_dimensions
   attr_accessible :owner_id, :item_active, :item_cost, :item_created_id, :item_description, :item_name, :item_notes, :item_part_no, :item_quantity_in_hand, :item_quantity_on_order, :item_revision, :item_revision_date, :item_tooling, :item_updated_id
   validates_presence_of :owner
 end
