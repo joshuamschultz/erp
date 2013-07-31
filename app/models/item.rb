335220e942b2aef0d205fe611289eb30e4566e0c
Joshua Schultz <joshuamschultz@gmail.com>
2013-07-31 17:17:06 +0530

    Modificatios in the item_alt_names

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :owner
+  has_many :item_alt_names
   attr_accessible :owner_id, :item_active, :item_cost, :item_created_id, :item_description, :item_name, :item_notes, :item_part_no, :item_quantity_in_hand, :item_quantity_on_order, :item_revision, :item_revision_date, :item_tooling, :item_updated_id
+  validates_presence_of :owner
 end
--- app/models/item_alt_name.rb
+++ app/models/item_alt_name.rb
@@ -1,4 +1,5 @@
 class ItemAltName < ActiveRecord::Base
   belongs_to :item
-  attr_accessible :item_alt_active, :item_alt_created_id, :item_alt_description, :item_alt_identifier, :item_alt_notes, :item_alt_updated_id
+  attr_accessible :item_id, :item_alt_active, :item_alt_created_id, :item_alt_description, :item_alt_identifier, :item_alt_notes, :item_alt_updated_id
+  validates_presence_of :item
 end
