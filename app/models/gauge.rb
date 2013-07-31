90b1101d30289dbb90025341c3fda4d0750b7a77
Joshua Schultz <joshuamschultz@gmail.com>
2013-07-31 17:17:48 +0530

    changes with contacts, gauges

--- app/models/gauge.rb
+++ app/models/gauge.rb
@@ -1,6 +1,7 @@
 class Gauge < ActiveRecord::Base  
-  attr_accessible :organization_id, :gage_caliberaion_period, :gage_caliberation_at, :gauge_active, 
-  :gauge_created_id, :gauge_tool_name, :gauge_tool_no, :gauge_updated_id
+  attr_accessible :organization_id, :gage_caliberaion_period, :gage_caliberation_last_at, 
+  :gage_caliberation_due_at, :gauge_active, :gauge_created_id, :gauge_tool_name, :gauge_tool_no, 
+  :gauge_updated_id
 
   after_initialize :default_values
 
@@ -8,6 +9,7 @@ class Gauge < ActiveRecord::Base
 	   self.gauge_active = true if self.gauge_active.nil?
   end
 
-  belongs_to :organization
+  belongs_to :organization, :conditions => ['organization_type_id = ?', MasterType.find_by_type_value("support").id]
+
   has_many :dimensions
 end
--- app/modules/common_actions.rb
+++ app/modules/common_actions.rb
@@ -75,42 +75,71 @@ module CommonActions
 
 		menus[:contacts] = {:class => "hasSubmenu glyphicons adress_book", :path => "#", :name => "Contacts", :type => "multiple"}
 		menus[:contacts][:sub_menu] = 	[
-											{:path => "#", :name => "Vendor"}, 
-											{:path => "#", :name => "Customer"},
-											{:path => "#", :name => "Support"}
+											{:path => contacts_path(org_type: "vendor", :contact_type => "contact"), :name => "Vendor"}, 
+											{:path => contacts_path(org_type: "customer", :contact_type => "contact"), :name => "Customer"},
+											{:path => contacts_path(org_type: "support", :contact_type => "contact"), :name => "Support"},
+											{:path => organizations_path, :name => "Organizations"}
 										]
 
-		menus[:quotes] = {:class => "hasSubmenu glyphicons notes", :path => "#", :name => "Quotes"}
+		menus[:quotes] = {:class => "hasSubmenu glyphicons notes", :path => "#", :name => "Quotes", :type => "multiple"}
 		menus[:quotes][:sub_menu] = 	[
 											{:path => "#", :name => "Vendor"}, 
 											{:path => "#", :name => "Customer"}
 										]
 
-		menus[:purchases] = {:class => "glyphicons cart_in", :path => "#", :name => "Purchases", :type => "single"}
+		menus[:purchases] = {:class => "hasSubmenu glyphicons cart_in", :path => "#", :name => "Purchases", :type => "multiple"}
+		menus[:purchases][:sub_menu] = 	[
+											{:path => "#", :name => "Purchase Orders"}, 
+											{:path => "#", :name => "PO Information"}
+										]		
 
-		menus[:sales] = {:class => "glyphicons stats", :path => "#", :name => "Sales", :type => "single"}
+		menus[:sales] = {:class => "hasSubmenu glyphicons stats", :path => "#", :name => "Sales", :type => "multiple"}
+		menus[:sales][:sub_menu] = 		[
+											{:path => "#", :name => "Sales Orders"}, 
+											{:path => "#", :name => "SO Information"}
+										]
 
-		menus[:inventory] = {:class => "glyphicons cargo", :path => "#", :name => "Inventory", :type => "single"}
+		menus[:inventory] = {:class => "hasSubmenu glyphicons cargo", :path => "#", :name => "Inventory", :type => "multiple"}
+		menus[:inventory][:sub_menu] = 	[
+											{:path => "#", :name => "Item"},
+											{:path => "#", :name => "Prints"},
+											{:path => materials_path, :name => "Materials"},
+											{:path => "#", :name => "Process"},
+											{:path => "#", :name => "Specifications"},
+											{:path => "#", :name => "Dimension Type"}
+										]
 
 		menus[:accounts] = {:class => "hasSubmenu glyphicons book", :path => "#", :name => "Accounts", :type => "multiple"}
-		menus[:accounts][:sub_menu] = 	[
-											{:path => "#", :name => "Receivables"}, 
+		menus[:accounts][:sub_menu] = 	[											
 											{:path => "#", :name => "Payables"},
-											{:path => "#", :name => "Payments"}
+											{:path => "#", :name => "Payments"},
+											{:path => "#", :name => "Receivables"}
 										]
 		
 		menus[:general_ledger] = {:class => "hasSubmenu glyphicons book_open", :path => "#", :name => "General Ledger", :type => "multiple"}
 		menus[:general_ledger][:sub_menu] = 	[
 											{:path => "#", :name => "Journal Entries"}, 
-											{:path => "#", :name => "Chart of Accounts"}
+											{:path => "#", :name => "Chart of Accounts"},
+											{:path => "#", :name => "Reconcile"}
 										]		
 
 		menus[:quality] = {:class => "hasSubmenu glyphicons log_book", :path => "#", :name => "Quality", :type => "multiple"}
 		menus[:quality][:sub_menu] = 	[
-											{:path => "#", :name => "Lots"}, 
-											{:path => "#", :name => "Instruments"},
-											{:path => "#", :name => "Quality Actions"},
-											{:path => "#", :name => "Customer Qualities"}
+											{:path => "#", :name => "Checklist"}, 
+											{:path => "#", :name => "Lot Info"},
+											{:path => "#", :name => "Dimensions"}, 
+											{:path => "#", :name => "Run at Rate"},
+											{:path => "#", :name => "Part Submission Warrant"}, 
+											{:path => "#", :name => "Packaging"},
+											{:path => gauges_path, :name => "Instruments"},
+											{:path => "#", :name => " Cause Analysis"}, 
+											{:path => process_types_path, :name => "Process Flow"},
+											{:path => "#", :name => "FMEA"},
+											{:path => "#", :name => "Control Plan"},
+											{:path => "#", :name => "Customer Response"},
+											{:path => "#", :name => "Quality Action"},
+											{:path => vendor_qualities_path, :name => "Quality ID"},
+											{:path => customer_qualities_path, :name => "Quality Level"}
 										]
 		
 		menus[:shipments] = {:class => "glyphicons boat", :path => "#", :name => "Shipments", :type => "single"}
@@ -127,17 +156,13 @@ module CommonActions
 
 		menus[:system] = {:class => "hasSubmenu glyphicons cogwheels", :path => "#", :name => "System", :type => "multiple"}
 		menus[:system][:sub_menu] = 	[
-											{:path => company_infos_path, :name => "Company Info"},											
-											{:path => "#", :name => "Privileges"},
-											{:path => "#", :name => "Documents"},
+											{:path => company_infos_path, :name => "Home Info"},
 											{:path => owners_path, :name => "Owners"},
-											{:path => materials_path, :name => "Materials"},
-											{:path => process_types_path, :name => "Processes"},
-											{:path => vendor_qualities_path, :name => "Quality ID"},
-											{:path => specifications_path, :name => "Specifications"},
-											{:path => commodities_path, :name => "Commodities"},
+											{:path => "#", :name => "Privileges"},
 											{:path => territories_path, :name => "Territories"},
-											{:path => organizations_path, :name => "Organizations"}
+											{:path => "#", :name => "Documents"},
+											{:path => specifications_path, :name => "Specifications"},
+											{:path => commodities_path, :name => "Commodities"}
 										]
 		menus
 	end
@@ -148,14 +173,13 @@ module CommonActions
 				:sub_menu => [	
 								{:path => company_infos_path, :name => "Company Info", :class => "", :drop_down => false, :sub_menu => []},
 								{:path => owners_path, :name => "Owners", :class => "", :drop_down => false, :sub_menu => []},
-								{:path => materials_path, :name => "Materials", :class => "", :drop_down => false, :sub_menu => []},
-								{:path => process_types_path, :name => "Processes", :class => "", :drop_down => false, :sub_menu => []},
-								{:path => vendor_qualities_path, :name => "Quality ID", :class => "", :drop_down => false, :sub_menu => []},
-								{:path => customer_qualities_path, :name => "Quality Level", :class => "", :drop_down => false, :sub_menu => []},
-								{:path => specifications_path, :name => "Specifications", :class => "", :drop_down => false, :sub_menu => []},
-								{:path => commodities_path, :name => "Commodities", :class => "", :drop_down => false, :sub_menu => []},
 								{:path => territories_path, :name => "Territories", :class => "", :drop_down => false, :sub_menu => []},
-								{:path => organizations_path, :name => "Organizations", :class => "", :drop_down => false, :sub_menu => []}
+								{:path => specifications_path, :name => "Specifications", :class => "", :drop_down => false, :sub_menu => []},
+								{:path => commodities_path, :name => "Commodities", :class => "", :drop_down => false, :sub_menu => []}
+								# {:path => materials_path, :name => "Materials", :class => "", :drop_down => false, :sub_menu => []},
+								# {:path => process_types_path, :name => "Processes", :class => "", :drop_down => false, :sub_menu => []},
+								# {:path => vendor_qualities_path, :name => "Quality ID", :class => "", :drop_down => false, :sub_menu => []},
+								# {:path => customer_qualities_path, :name => "Quality Level", :class => "", :drop_down => false, :sub_menu => []},
 							]					
 			}
 		]		
