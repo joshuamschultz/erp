a170478dfea73666e7508f51600cce611bbff939
Joshua Schultz <joshuamschultz@gmail.com>
2013-07-31 21:21:40 +0530

    Modification in item_part_dimensions

--- app/models/item_part_dimension.rb
+++ app/models/item_part_dimension.rb
@@ -1,4 +1,5 @@
 class ItemPartDimension < ActiveRecord::Base
   belongs_to :item
-  attr_accessible :item_part_active, :item_part_created_id, :item_part_critical, :item_part_letter, :item_part_neg_tolerance, :item_part_notes, :item_part_pos_tolerance, :item_part_type, :item_part_updated_id
+  attr_accessible :item_id, :item_part_active, :item_part_created_id, :item_part_critical, :item_part_letter, :item_part_neg_tolerance, :item_part_notes, :item_part_pos_tolerance, :item_part_type, :item_part_updated_id
+  validates_presence_of :item
 end
