da4a281c0a4b1c372aee97986dd9f7659eb3f37d
Joshua Schultz <joshuamschultz@gmail.com>
2013-08-07 19:13:09 +0530

    po form added

---
+++ db/migrate/20130807104443_create_po_headers.rb
@@ -0,0 +1,19 @@
+class CreatePoHeaders < ActiveRecord::Migration
+  def change
+    create_table :po_headers do |t|
+      t.integer :po_type_id
+      t.references :organization
+      t.string :po_identifier
+      t.string :po_description
+      t.decimal :po_total
+      t.string :po_status
+      t.text :po_notes
+      t.boolean :po_active
+      t.integer :po_created_id
+      t.integer :po_updated_id
+
+      t.timestamps
+    end
+    add_index :po_headers, :organization_id
+  end
+end
