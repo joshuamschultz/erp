c22e36c4853bf7f8968198d6c37a479fe7daae52
Joshua Schultz <joshuamschultz@gmail.com>
2013-08-21 18:08:30 +0530

    asset and link changes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -76,6 +76,7 @@ class Organization < ActiveRecord::Base
 	has_many :comments, :as => :commentable, :dependent => :destroy
 	has_many :contacts, :as => :contactable, :dependent => :destroy
 	has_many :organization_processes, :dependent => :destroy
+	has_many :process_types, :through => :organization_processes
 	has_many :gauges, :dependent => :destroy
 	has_many :item_revisions
 	has_many :po_headers, :dependent => :destroy #For vendor
