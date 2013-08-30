57e8e5666cc8c0b156bc4e650deae9676138a9c6
praveen-agileblaze <praveenm@agileblaze.com>
2013-08-30 22:04:32 +0530

    layout modification in fmea module

--- app/models/fmea_type.rb
+++ app/models/fmea_type.rb
@@ -1,3 +1,28 @@
 class FmeaType < ActiveRecord::Base
-  attr_accessible :fmea_active, :fmea_created_id, :fmea_description, :fmea_name, :fmea_notes, :fmea_updated_id
+  include Rails.application.routes.url_helpers
+
+  attr_accessible :fmea_active, :fmea_created_id, :fmea_description, 
+       :fmea_name, :fmea_notes, :fmea_updated_id,:attachment_attributes
+
+  after_initialize :default_values
+
+  def default_values
+    self.fmea_active = true if self.fmea_active.nil?
+  end
+
+  has_one :attachment, :as => :attachable, :dependent => :destroy
+
+  accepts_nested_attributes_for :attachment, :allow_destroy => true
+
+
+  def redirect_path
+      process_type_path(self)
+  end
+
+  before_save :before_save_values
+
+  def before_save_values
+      self.fmea_name = self.attachment.attachment_name
+  end
+  
 end
