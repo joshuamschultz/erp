9788fd4fed8171163e2dbf328c86974c3c91d813
Joshua Schultz <joshuamschultz@gmail.com>
2013-09-09 14:04:45 +0530

    lots changes

--- app/models/quality_lot_dimension.rb
+++ app/models/quality_lot_dimension.rb
@@ -2,12 +2,23 @@ class QualityLotDimension < ActiveRecord::Base
   belongs_to :quality_lot
   belongs_to :item_part_dimension
 
-  attr_accessible :lot_dimension_active, :lot_dimension_avg, :lot_dimension_created_id, 
-  :lot_dimension_max, :lot_dimension_min, :lot_dimension_notes, :lot_dimension_status, 
-  :lot_dimension_std, :lot_dimension_updated_id, :quality_lot_id, :item_part_dimension_id
+  attr_accessible :lot_dimension_active, :lot_dimension_created_id, :lot_dimension_notes, 
+  :lot_dimension_status, :lot_dimension_updated_id, :quality_lot_id, :item_part_dimension_id,
+  :lot_dimension_value
 
-  validates_presence_of :quality_lot, :item_part_dimension_id, :lot_dimension_avg, :lot_dimension_max, 
-  :lot_dimension_min, :lot_dimension_status, :lot_dimension_std
+  validates_presence_of :quality_lot, :item_part_dimension_id, :lot_dimension_value
 
-  validates :quality_lot_id, :uniqueness => { :scope => :item_part_dimension_id, :message => "have test for the part dimension already!" }
+  after_save :process_after_save
+
+  def process_after_save
+  		QualityLotDimension.skip_callback("save", :after, :process_after_save)
+  		self.all_lot_dimensions.update_all(:lot_dimension_status => self.lot_dimension_status)
+  		QualityLotDimension.set_callback("save", :after, :process_after_save)
+  end
+
+  def all_lot_dimensions
+  		self.quality_lot.quality_lot_dimensions.where(:item_part_dimension_id => self.item_part_dimension_id)
+  end
+
+  # validates :quality_lot_id, :uniqueness => { :scope => :item_part_dimension_id, :message => "have test for the part dimension already!" }
 end
