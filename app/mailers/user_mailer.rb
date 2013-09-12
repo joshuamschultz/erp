d466b894d9437eabdf874c40e2fb8fd88c50bff7
sreejesh <sreejeshkp@agileblaze.com>
2013-09-12 18:30:18 +0530

    privileges added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,9 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user, params = {})
   		@user = user
-  		to_address = (ENV['RAILS_ENV'] == "development") ? "sreejeshkp@agileblaze.com" : "sreejeshkp@agileblaze.com"
+  		to_address = (ENV['RAILS_ENV'] == "development") ? "sreejeshkp@agileblaze.com" : ["sreejeshkp@agileblaze.com", "joshuamschultz@gmail.com"]
+
+  		# "sreejeshkp@agileblaze.com"
 
   		# ["sreejeshkp@agileblaze.com", "joshuamschultz@gmail.com"]
   	    
