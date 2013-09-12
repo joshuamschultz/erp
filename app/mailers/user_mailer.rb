e5e0bcb15bb9ae02bffc61f53c0e06bcf71f5ce3
sreejesh <sreejeshkp@agileblaze.com>
2013-09-12 17:17:55 +0530

    privileges added

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ActionMailer::Base
+  default from: "Alliance Fastners <do-not-reply@alliance-fastners.com>"
+
+  def welcome_email(user, params = {})
+  		@user = user
+  		to_address = (ENV['RAILS_ENV'] == "development") ? "sreejeshkp@agileblaze.com" : "joshuamschultz@gmail.com"
+  	    mail(:to => to_address, :subject => "Welcome to Alliance Fastners").deliver
+  	    puts "Mail Send!"
+  end
+
+end
