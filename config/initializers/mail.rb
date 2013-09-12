efcdee0d141d9720e295225ea0c5493959ed4920
sreejesh <sreejeshkp@agileblaze.com>
2013-09-12 18:04:23 +0530

    privileges added

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,6 +3,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
 	:address =>'smtp.gmail.com',
     :port => 587,
+    :domain => "smtp.gmail.com",
     :user_name => 'eventsparks@gmail.com',
     :password => 'agileblaze525',
     :authentication => 'plain',
