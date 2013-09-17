be7b17b0eea6efd428fcdd0f2c3d67d006acf0b7
Joshua Schultz <joshuamschultz@gmail.com>
2013-09-17 14:01:33 +0530

    fixed scaling

--- app/controllers/quality_lot_dimensions_controller.rb
+++ app/controllers/quality_lot_dimensions_controller.rb
@@ -8,10 +8,15 @@ class QualityLotDimensionsController < ApplicationController
   # GET /quality_lot_dimensions
   # GET /quality_lot_dimensions.json
   def index
-    @quality_lot = QualityLot.find(params[:quality_lot_id])
-    @item_revision = @quality_lot.item_revision
-    @item = @item_revision.item
-    @quality_lot_dimensions = @quality_lot.quality_lot_dimensions.group(:quality_lot_id, :item_part_dimension_id)
+    @quality_lot = QualityLot.find(params[:quality_lot_id]) if params[:quality_lot_id]
+    @quality_lot = QualityLot.first unless @quality_lot
+    if @quality_lot
+        @item_revision = @quality_lot.item_revision
+        @item = @item_revision.item
+        @quality_lot_dimensions = @quality_lot.quality_lot_dimensions.group(:quality_lot_id, :item_part_dimension_id)
+    else
+        @quality_lot_dimensions = []
+    end
 
     respond_to do |format|
       format.html # index.html.erb
@@ -20,8 +25,8 @@ class QualityLotDimensionsController < ApplicationController
               lot_dimension[:lot_control_no] = CommonActions.linkable(quality_lot_path(lot_dimension.quality_lot), lot_dimension.quality_lot.lot_control_no)
               lot_dimension[:item_part_letter] = CommonActions.linkable(item_item_revision_item_part_dimension_path(lot_dimension.item_part_dimension.item_revision.item, lot_dimension.item_part_dimension.item_revision, lot_dimension.item_part_dimension), lot_dimension.item_part_dimension.item_part_letter)
               lot_dimension[:item_part_dimensions] = lot_dimension.item_part_dimension.item_part_dimension #CommonActions.linkable(item_item_revision_item_part_dimension_path(lot_dimension.item_part_dimension.item_revision.item, lot_dimension.item_part_dimension.item_revision, lot_dimension.item_part_dimension), lot_dimension.item_part_dimension.item_part_letter)
-              lot_dimension[:item_part_pos_tolerance] = lot_dimension.item_part_dimension.item_part_dimension.to_f + lot_dimension.item_part_dimension.item_part_pos_tolerance.to_f
-              lot_dimension[:item_part_neg_tolerance] = lot_dimension.item_part_dimension.item_part_dimension.to_f - lot_dimension.item_part_dimension.item_part_neg_tolerance.to_f
+              lot_dimension[:item_part_pos_tolerance] = lot_dimension.item_part_dimension.item_part_dimension + lot_dimension.item_part_dimension.item_part_pos_tolerance
+              lot_dimension[:item_part_neg_tolerance] = lot_dimension.item_part_dimension.item_part_dimension - lot_dimension.item_part_dimension.item_part_neg_tolerance
 
               # lot_dimension_links = []
               # lot_dimension_links <<  {:name => "Accept", :method => "put", :path => quality_lot_dimension_path(lot_dimension, status: "accepted") } if lot_dimension.lot_dimension_status == "rejected" || lot_dimension.lot_dimension_status.nil?
@@ -29,14 +34,14 @@ class QualityLotDimensionsController < ApplicationController
               # lot_dimension[:links] = CommonActions.object_crud_paths(nil, edit_quality_lot_dimension_path(lot_dimension, quality_lot_id: @quality_lot.id), nil)
               # "%0.6f" % (
 
-              lot_dimension[:lot_dimension_avg] = lot_dimension.all_lot_dimensions.sum(:lot_dimension_value)/lot_dimension.all_lot_dimensions.count
+              lot_dimension[:lot_dimension_avg] = (lot_dimension.all_lot_dimensions.sum(:lot_dimension_value)/lot_dimension.all_lot_dimensions.count).round(6)
               
               lot_dimension_values = []
               lot_dimension.all_lot_dimensions.collect(&:lot_dimension_value).each do |value|
                 lot_dimension_values << value.to_i
               end
 
-              lot_dimension[:lot_dimension_std] = lot_dimension_values.stdev rescue 0
+              lot_dimension[:lot_dimension_std] = lot_dimension_values.stdev.round(6) rescue 0
               lot_dimension[:lot_dimension_max] = lot_dimension.all_lot_dimensions.maximum(:lot_dimension_value)
               lot_dimension[:lot_dimension_min] = lot_dimension.all_lot_dimensions.minimum(:lot_dimension_value)
           end
