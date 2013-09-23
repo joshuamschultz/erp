493959f2b492dca16dc2d304a04e29b2835c5a94
sreejesh <sreejeshkp@agileblaze.com>
2013-09-23 17:10:33 +0530

    receipts added

--- db/migrate/20130923105522_create_receipt_lines.rb
+++ db/migrate/20130923105522_create_receipt_lines.rb
@@ -2,7 +2,7 @@ class CreateReceiptLines < ActiveRecord::Migration
   def change
     create_table :receipt_lines do |t|
       t.references :receipt
-      t.decimal :receipt_line_amount
+      t.decimal :receipt_line_amount, :precision => 25, :scale => 10, :default => 0
       t.integer :receipt_line_created_id
       t.integer :receipt_line_updated_id
 
