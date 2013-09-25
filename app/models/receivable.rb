3cea2f5a3d578678f027de05832717e6701b4a66
praveen-agileblaze <praveenm@agileblaze.com>
2013-09-25 21:57:10 +0530

    changes in receipt and receivable form

--- app/models/receivable.rb
+++ app/models/receivable.rb
@@ -1,10 +1,11 @@
 class Receivable < ActiveRecord::Base
+  belongs_to :organization, :conditions => ['organization_type_id = ?', MasterType.find_by_type_value("customer").id]
+  belongs_to :so_header
   has_many :receivable_shipments, :dependent => :destroy
   has_many :receivable_lines, :dependent => :destroy
-  # has_many :receipt_lines, :dependent => :destroy
+  has_many :receipt_lines, :dependent => :destroy
 
-  belongs_to :organization, :conditions => ['organization_type_id = ?', MasterType.find_by_type_value("customer").id]  
-  belongs_to :so_header
+  
 
   attr_accessible :receivable_active, :receivable_cost, :receivable_created_id,
   :receivable_discount, :receivable_identifier, :receivable_notes, :receivable_status, 
@@ -64,7 +65,7 @@ class Receivable < ActiveRecord::Base
   end
 
   def receivable_current_balance
-      self.receivable_total - self.receivable_lines.sum(:receivable_line_amount)
+      self.receivable_total - self.receivable_lines.sum(:receivable_line_cost)
   end
 
 end
--- db/migrate/20130923105522_create_receipt_lines.rb
+++ db/migrate/20130923105522_create_receipt_lines.rb
@@ -2,12 +2,14 @@ class CreateReceiptLines < ActiveRecord::Migration
   def change
     create_table :receipt_lines do |t|
       t.references :receipt
+      t.references :receivable
       t.decimal :receipt_line_amount, :precision => 25, :scale => 10, :default => 0
       t.integer :receipt_line_created_id
       t.integer :receipt_line_updated_id
 
       t.timestamps
-    end
+    end 
     add_index :receipt_lines, :receipt_id
+    add_index :receipt_lines, :receivable_id
   end
 end
