864c317fa83d3889217bc3fadff15fe9f12b8a17
praveen-agileblaze <praveenm@agileblaze.com>
2013-09-26 20:57:54 +0530

    Added funtionality in quote and quote line

--- app/controllers/quote_lines_controller.rb
+++ app/controllers/quote_lines_controller.rb
@@ -1,13 +1,24 @@
 class QuoteLinesController < ApplicationController
-  # GET quotes/1/quote_lines
-  # GET quotes/1/quote_lines.json
+before_filter :set_autocomplete_values, only: [:create, :update]
+
+  def set_autocomplete_values    
+    params[:quote_line][:item_alt_name_id], params[:alt_name_id] = params[:alt_name_id], params[:quote_line][:item_alt_name_id]
+    params[:quote_line][:item_alt_name_id] = params[:org_alt_name_id] if params[:quote_line][:item_alt_name_id] == "" || params[:quote_line][:item_alt_name_id].nil?
+  end
+  
   def index
     @quote = Quote.find(params[:quote_id])
     @quote_lines = @quote.quote_lines
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render :json => @quote_lines }
+      format.json { 
+        @quote_lines = @quote_lines.select{|quote_line|
+              quote_line[:item_part_no] = CommonActions.linkable(item_path(quote_line.item), quote_line.item_alt_name.item_alt_identifier)
+              quote_line[:links] = CommonActions.object_crud_paths(nil, edit_quote_quote_line_path(@quote, quote_line), nil)
+          }
+          render json: {:aaData => @quote_lines}
+       }
     end
   end
 
@@ -49,7 +60,7 @@ class QuoteLinesController < ApplicationController
 
     respond_to do |format|
       if @quote_line.save
-        format.html { redirect_to([@quote_line.quote, @quote_line], :notice => 'Quote line was successfully created.') }
+        format.html { redirect_to new_quote_quote_line_path(@quote), :notice => 'Quote line was successfully created.' }
         format.json { render :json => @quote_line, :status => :created, :location => [@quote_line.quote, @quote_line] }
       else
         format.html { render :action => "new" }
@@ -66,7 +77,7 @@ class QuoteLinesController < ApplicationController
 
     respond_to do |format|
       if @quote_line.update_attributes(params[:quote_line])
-        format.html { redirect_to([@quote_line.quote, @quote_line], :notice => 'Quote line was successfully updated.') }
+        format.html { redirect_to new_quote_quote_line_path(@quote), :notice => 'Quote line was successfully updated.'}
         format.json { head :ok }
       else
         format.html { render :action => "edit" }
@@ -83,7 +94,7 @@ class QuoteLinesController < ApplicationController
     @quote_line.destroy
 
     respond_to do |format|
-      format.html { redirect_to quote_quote_lines_url(quote) }
+      format.html { redirect_to new_quote_quote_line_path(@quote) }
       format.json { head :ok }
     end
   end
