8f7855fddc7259e4dce682c59638d1b2ef0ec487
Joshua Schultz <joshuamschultz@gmail.com>
2013-10-18 10:51:31 +0530

    fixes

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,11 +3,15 @@ class AccountController < ApplicationController
   def dashboard
 	@menus[:dashboard][:active] = "active"
 
+
+
+	# @name = "Sree"
 	# html = render_to_string(:action => "tester", :layout => false)
 	# kit = PDFKit.new(html)
-	# kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/test.css"
-	# file = kit.to_file("#{Rails.root}/public/test.pdf")
-	# send_data(kit.to_pdf, :filename => 'test.pdf', :type => 'application/pdf',:disposition  => "inline",  :orientation => 'Landscape')
+	# # kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/test.css"
+	# # file = kit.to_file("#{Rails.root}/public/test.pdf")
+	# # send_data(kit.to_pdf, :filename => 'test.pdf', :type => 'application/pdf', :disposition  => "inline", :orientation => 'A4')
+	# send_data(kit.to_pdf, :filename => 'test.pdf', :type => 'application/pdf', :disposition  => "inline")
   end
 
   def tester  		
--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,11 +1,11 @@
-# PDFKit.configure do |config|
-#   config.default_options = {
-#     :encoding=>"UTF-8",
-#     :page_size=>"A4",
-#     :margin_top=>"0.25in",
-#     :margin_right=>"0.1in",
-#     :margin_bottom=>"0.25in",
-#     :margin_left=>"0.1in",
-#     :disable_smart_shrinking=> false
-#   }
-# end
+PDFKit.configure do |config|
+  config.default_options = {
+    :encoding=>"UTF-8",
+    :page_size=>"A4",
+    :margin_top=>"0.5in",
+    :margin_right=>"0.3in",
+    :margin_bottom=>"0.5in",
+    :margin_left=>"0.3in",
+    :disable_smart_shrinking=> false
+  }
+end
