53a5b86774e7f5b9e7b9d3f2f8112e5e0d7c3122
sreejesh <sreejeshkp@agileblaze.com>
2013-10-21 17:13:40 +0530

    fixes

--- db/migrate/20131018105538_add_gauge_trial.rb
+++ db/migrate/20131018105538_add_gauge_trial.rb
@@ -1,5 +1,6 @@
 class AddGaugeTrial < ActiveRecord::Migration
   def change
   	add_column :quality_lot_gauge_results, :lot_gauge_result_trial, :integer
+  	add_column :quality_lot_gauge_results, :lot_gauge_result_row, :integer
   end
 end
