98052605ef552b60cf821ab3420b241ef2e4b984
sreejesh <sreejeshkp@agileblaze.com>
2013-10-22 16:21:35 +0530

    completed fixes with item and po_line

--- app/modules/common_actions.rb
+++ app/modules/common_actions.rb
@@ -7,8 +7,10 @@ module CommonActions
       	paths += "<a href='#{edit_path}' class='btn-action glyphicons pencil btn-success'><i></i></a> " if edit_path
       	paths += "<a href='#{delete_path}' class='btn-action glyphicons remove_2 btn-danger' rel='nofollow' data-method='delete' data-confirm='Are you sure?'><i></i></a> " if delete_path
 		others.each do |other|
-			other[:method] ||= "get"
-			paths += "<a href='#{other[:path]}' class='btn btn-mini btn-orange' data-method='#{other[:method]}'>#{other[:name]}</a> "
+			unless other.nil?
+				other[:method] ||= "get"
+				paths += "<a href='#{other[:path]}' class='btn btn-mini btn-orange' data-method='#{other[:method]}'>#{other[:name]}</a> "
+			end
 		end
 		paths
 	end
