90c982094da060e427baa49d3f2c226812b55e0d
sreejesh <sreejeshkp@agileblaze.com>
2013-11-06 10:43:24 +0530

    changes with po_shipments

--- app/models/po_shipment.rb
+++ app/models/po_shipment.rb
@@ -1,5 +1,8 @@
 class PoShipment < ActiveRecord::Base
   belongs_to :po_line
+
+  has_one :payable_po_shipment
+  has_one :payable, through: :payable_po_shipment
   
   attr_accessible :po_line_id, :po_shipment_created_id, :po_shipment_updated_id, 
   :po_shipped_count, :po_shipped_cost, :po_shipped_shelf, :po_shipped_unit
---
+++ db/migrate/20131025101331_add_so_to_po_header.rb
@@ -0,0 +1,4 @@
+class AddSoToPoHeader < ActiveRecord::Migration
+  def change
+  end
+end
