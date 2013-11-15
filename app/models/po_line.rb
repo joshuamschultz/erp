ab8247014891bcd8419c3d92133e6ea1e85bf80d
sreejesh <sreejeshkp@agileblaze.com>
2013-11-15 19:25:29 +0530

    shipping changes

--- app/models/po_line.rb
+++ app/models/po_line.rb
@@ -72,9 +72,9 @@ class PoLine < ActiveRecord::Base
       object[:po_identifier] += "<a onclick='process_all_open(#{po_line.po_header.id}, $(this)); return false' class='pull-right btn btn-small btn-success' href='#'>Receive All</a>"
       
       object[:links] = "<a po_line_id='#{po_line.id}' po_shipped_status='rejected' class='pull-right btn_save_shipped btn-action glyphicons ban btn-danger' href='#'><i></i></a> "
-      object[:links] += "   <a po_line_id='#{po_line.id}' po_shipped_status='on hold' class='pull-right btn_save_shipped btn-action glyphicons circle_exclamation_mark btn-warning' href='#'><i></i></a> "
-      object[:links] += "   <a po_line_id='#{po_line.id}' po_shipped_status='received' class='pull-right btn_save_shipped btn-action glyphicons check btn-success' href='#'><i></i></a> "
-      object[:links] += "   <div class='pull-right shipping_status'></div>"
+      object[:links] += " <a po_line_id='#{po_line.id}' po_shipped_status='on hold' class='pull-right btn_save_shipped btn-action glyphicons circle_exclamation_mark btn-warning' href='#'><i></i></a> "
+      object[:links] += " <a po_line_id='#{po_line.id}' po_shipped_status='received' class='pull-right btn_save_shipped btn-action glyphicons check btn-success' href='#'><i></i></a> "
+      object[:links] += " <div class='pull-right shipping_status'></div>"
     end
     object
   end
