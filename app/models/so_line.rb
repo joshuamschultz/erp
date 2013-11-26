3fa46a4465a2c3f858acb989802bbb67def25d80
sreejesh <sreejeshkp@agileblaze.com>
2013-11-26 19:27:04 +0530

    po changes

--- app/models/so_line.rb
+++ app/models/so_line.rb
@@ -9,9 +9,10 @@ class SoLine < ActiveRecord::Base
 
   attr_accessible :so_line_cost, :so_line_created_id, :so_line_freight, :so_line_price, :so_line_quantity, 
   :so_line_status, :so_line_updated_id, :organization_id, :item_id, :so_header_id, :item_alt_name_id,
-  :so_line_notes, :so_line_active, :vendor_quality_id, :customer_quality_id, :so_line_shipped, :so_line_sell
+  :so_line_notes, :so_line_active, :vendor_quality_id, :customer_quality_id, :so_line_shipped, :so_line_sell,
+  :so_line_vendor_po
 
-  validates_presence_of :so_header, :organization, :item_alt_name, :so_line_cost, :so_line_quantity, :customer_quality
+  validates_presence_of :so_header, :item_alt_name, :so_line_cost, :so_line_quantity, :customer_quality
 
   validates_numericality_of :so_line_cost, :so_line_quantity, :so_line_sell
 
