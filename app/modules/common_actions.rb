750b1e3b44ffc8563267c2a414618532741625eb
sreejesh <sreejeshkp@agileblaze.com>
2014-01-08 18:45:19 +0530

    gl changes

--- app/modules/common_actions.rb
+++ app/modules/common_actions.rb
@@ -145,7 +145,8 @@ module CommonActions
 		menus[:general_ledger] = {:class => "hasSubmenu glyphicons book_open", :path => "#", :name => "General Ledger", :type => "multiple"}
 		menus[:general_ledger][:sub_menu] = 	[
 											{:path => "#", :name => "Journal Entries"}, 
-											{:path => gl_accounts_path, :name => "Chart of Accounts"},
+											{:path => gl_types_path, :name => "GL Types"},
+											{:path => gl_accounts_path, :name => "GL Accounts"},
 											{:path => "#", :name => "Reconcile"}
 										]		
 
