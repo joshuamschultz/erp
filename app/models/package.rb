29dff83688a0d0aac4d7ead853bae06bbfd81504
Joshua Schultz <joshuamschultz@gmail.com>
2014-05-21 03:05:12 +0530

    added package material

--- app/models/package.rb
+++ app/models/package.rb
@@ -6,7 +6,14 @@ class Package < ActiveRecord::Base
   	:load_shipped_width, :package_comment, :pallet_height, :pallet_length, :pallet_tare_weight, :pallet_width, 
   	:part_content_type, :part_file_name, :part_file_size, :part_size_height, :part_size_length, :part_size_width, 
   	:part_updated_at, :part_weight, :quantity_per_container, :unit_load_content_type, :unit_load_file_name, 
-  	:unit_load_file_size, :unit_load_gross, :unit_load_updated_at, :quality_lot_id, :part, :container, :dunnage, :unit_load
+  	:unit_load_file_size, :unit_load_gross, :unit_load_updated_at, :quality_lot_id, :part, :container, :dunnage, :unit_load,
+  	:dunnage_manufacturer ,:container_color_manufacturer ,:container_type_manufacturer ,:cover_cap_manufacturer ,:pallet_manufacturer ,
+  	:strech_shrink_manufacturer ,:banding_manufacturer ,:other_manufacturer,:dunnage_material , :container_color_material ,:container_type_material ,
+  	:cover_cap_material ,:pallet_material ,:strech_shrink_material ,:banding_material ,:other_material, :dunnage_lead_time ,:container_color_lead_time ,
+  	:container_type_lead_time ,:cover_cap_lead_time ,:pallet_lead_time ,:strech_shrink_lead_time ,:banding_lead_time ,:other_lead_time, :dunnage_ret_exp ,
+  	:container_color_ret_exp ,:container_type_ret_exp ,:cover_cap_ret_exp ,:pallet_ret_exp ,:strech_shrink_ret_exp ,:banding_ret_exp ,:other_ret_exp,
+  	:dunnage_comment ,:container_color_comment ,:container_type_comment ,:cover_cap_comment ,:pallet_comment ,:strech_shrink_comment ,:banding_comment ,:other_comment
+
 
 
   	belongs_to :quality_lot
---
+++ db/migrate/20140520133925_add_package_material_to_package.rb
@@ -0,0 +1,48 @@
+class AddPackageMaterialToPackage < ActiveRecord::Migration
+  def change
+  	add_column :packages, :dunnage_manufacturer, :text, :default => ""
+  	add_column :packages, :container_color_manufacturer, :text, :default => ""
+  	add_column :packages, :container_type_manufacturer, :text, :default => ""
+  	add_column :packages, :cover_cap_manufacturer, :text, :default => ""
+  	add_column :packages, :pallet_manufacturer, :text, :default => ""
+  	add_column :packages, :strech_shrink_manufacturer, :text, :default => ""
+  	add_column :packages, :banding_manufacturer, :text, :default => ""
+  	add_column :packages, :other_manufacturer, :text, :default => ""
+
+  	add_column :packages, :dunnage_material, :text, :default => ""
+  	add_column :packages, :container_color_material, :text, :default => ""
+  	add_column :packages, :container_type_material, :text, :default => ""
+  	add_column :packages, :cover_cap_material, :text, :default => ""
+  	add_column :packages, :pallet_material, :text, :default => ""
+  	add_column :packages, :strech_shrink_material, :text, :default => ""
+  	add_column :packages, :banding_material, :text, :default => ""
+  	add_column :packages, :other_material, :text, :default => ""
+
+  	add_column :packages, :dunnage_lead_time, :text, :default => ""
+  	add_column :packages, :container_color_lead_time, :text, :default => ""
+  	add_column :packages, :container_type_lead_time, :text, :default => ""
+  	add_column :packages, :cover_cap_lead_time, :text, :default => ""
+  	add_column :packages, :pallet_lead_time, :text, :default => ""
+  	add_column :packages, :strech_shrink_lead_time, :text, :default => ""
+  	add_column :packages, :banding_lead_time, :text, :default => ""
+  	add_column :packages, :other_lead_time, :text, :default => ""
+
+  	add_column :packages, :dunnage_ret_exp, :text, :default => ""
+  	add_column :packages, :container_color_ret_exp, :text, :default => ""
+  	add_column :packages, :container_type_ret_exp, :text, :default => ""
+  	add_column :packages, :cover_cap_ret_exp, :text, :default => ""
+  	add_column :packages, :pallet_ret_exp, :text, :default => ""
+  	add_column :packages, :strech_shrink_ret_exp, :text, :default => ""
+  	add_column :packages, :banding_ret_exp, :text, :default => ""
+  	add_column :packages, :other_ret_exp, :text, :default => ""
+
+  	add_column :packages, :dunnage_comment, :text, :default => ""
+  	add_column :packages, :container_color_comment, :text, :default => ""
+  	add_column :packages, :container_type_comment, :text, :default => ""
+  	add_column :packages, :cover_cap_comment, :text, :default => ""
+  	add_column :packages, :pallet_comment, :text, :default => ""
+  	add_column :packages, :strech_shrink_comment, :text, :default => ""
+  	add_column :packages, :banding_comment, :text, :default => ""
+  	add_column :packages, :other_comment, :text, :default => ""
+  end
+end
