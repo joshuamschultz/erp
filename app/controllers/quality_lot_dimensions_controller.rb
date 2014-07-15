e9ef262215103056c858582662cac5e53bc55126
Joshua Schultz <joshuamschultz@gmail.com>
2014-07-16 02:10:10 +0530

    hidded the value for gonogo

--- app/controllers/quality_lot_dimensions_controller.rb
+++ app/controllers/quality_lot_dimensions_controller.rb
@@ -22,11 +22,12 @@ class QualityLotDimensionsController < ApplicationController
       format.html # index.html.erb
       format.json { 
           @quality_lot_dimensions.each do |lot_dimension|
-              lot_dimension[:lot_control_no] = CommonActions.linkable(quality_lot_path(lot_dimension.quality_lot), lot_dimension.quality_lot.lot_control_no)
-              lot_dimension[:item_part_letter] = CommonActions.linkable(item_item_revision_item_part_dimension_path(lot_dimension.item_part_dimension.item_revision.item, lot_dimension.item_part_dimension.item_revision, lot_dimension.item_part_dimension), lot_dimension.item_part_dimension.item_part_letter)
-              lot_dimension[:item_part_dimensions] = lot_dimension.item_part_dimension.item_part_dimension.round(4) #CommonActions.linkable(item_item_revision_item_part_dimension_path(lot_dimension.item_part_dimension.item_revision.item, lot_dimension.item_part_dimension.item_revision, lot_dimension.item_part_dimension), lot_dimension.item_part_dimension.item_part_letter)
-              lot_dimension[:item_part_pos_tolerance] = (lot_dimension.item_part_dimension.item_part_dimension + lot_dimension.item_part_dimension.item_part_pos_tolerance).to_f.round(4)
-              lot_dimension[:item_part_neg_tolerance] = (lot_dimension.item_part_dimension.item_part_dimension - lot_dimension.item_part_dimension.item_part_neg_tolerance).to_f.round(4)
+              go_non_go_status = lot_dimension.item_part_dimension.go_non_go
+              lot_dimension[:lot_control_no] = go_non_go_status ? " " : CommonActions.linkable(quality_lot_path(lot_dimension.quality_lot), lot_dimension.quality_lot.lot_control_no)
+              lot_dimension[:item_part_letter] =  CommonActions.linkable(item_item_revision_item_part_dimension_path(lot_dimension.item_part_dimension.item_revision.item, lot_dimension.item_part_dimension.item_revision, lot_dimension.item_part_dimension), lot_dimension.item_part_dimension.item_part_letter)
+              lot_dimension[:item_part_dimensions] =  go_non_go_status ? " " : lot_dimension.item_part_dimension.item_part_dimension.round(4) #CommonActions.linkable(item_item_revision_item_part_dimension_path(lot_dimension.item_part_dimension.item_revision.item, lot_dimension.item_part_dimension.item_revision, lot_dimension.item_part_dimension), lot_dimension.item_part_dimension.item_part_letter)
+              lot_dimension[:item_part_pos_tolerance] = go_non_go_status ? " " : (lot_dimension.item_part_dimension.item_part_dimension + lot_dimension.item_part_dimension.item_part_pos_tolerance).to_f.round(4)
+              lot_dimension[:item_part_neg_tolerance] = go_non_go_status ? " " : (lot_dimension.item_part_dimension.item_part_dimension - lot_dimension.item_part_dimension.item_part_neg_tolerance).to_f.round(4)
 
               # lot_dimension_links = []
               # lot_dimension_links <<  {:name => "Accept", :method => "put", :path => quality_lot_dimension_path(lot_dimension, status: "accepted") } if lot_dimension.lot_dimension_status == "rejected" || lot_dimension.lot_dimension_status.nil?
@@ -34,16 +35,16 @@ class QualityLotDimensionsController < ApplicationController
               # lot_dimension[:links] = CommonActions.object_crud_paths(nil, edit_quality_lot_dimension_path(lot_dimension, quality_lot_id: @quality_lot.id), nil)
               # "%0.6f" % (
 
-              lot_dimension[:lot_dimension_avg] = (lot_dimension.all_lot_dimensions.sum(:lot_dimension_value)/lot_dimension.all_lot_dimensions.count).to_f.round(4)
+              lot_dimension[:lot_dimension_avg] = go_non_go_status ? " " : (lot_dimension.all_lot_dimensions.sum(:lot_dimension_value)/lot_dimension.all_lot_dimensions.count).to_f.round(4)
               
               lot_dimension_values = []
               lot_dimension.all_lot_dimensions.collect(&:lot_dimension_value).each do |value|
                   lot_dimension_values << value.to_f
               end
 
-              lot_dimension[:lot_dimension_std] = lot_dimension_values.stdev.round(4) rescue 0
-              lot_dimension[:lot_dimension_max] = lot_dimension.all_lot_dimensions.maximum(:lot_dimension_value).to_f.round(4)
-              lot_dimension[:lot_dimension_min] = lot_dimension.all_lot_dimensions.minimum(:lot_dimension_value).to_f.round(4)
+              lot_dimension[:lot_dimension_std] = go_non_go_status ? " " : (lot_dimension_values.stdev.round(4) rescue 0) 
+              lot_dimension[:lot_dimension_max] = go_non_go_status ? " " : lot_dimension.all_lot_dimensions.maximum(:lot_dimension_value).to_f.round(4)
+              lot_dimension[:lot_dimension_min] = go_non_go_status ? " " : lot_dimension.all_lot_dimensions.minimum(:lot_dimension_value).to_f.round(4)
           end
           render json: { :aaData => @quality_lot_dimensions } 
       }
