fd9ee091b9a9e596f744057e9fc573d1a4165f41
manager <mohanraj@agileblaze.com>
2014-09-09 12:36:55 +0530

    Revert "For arcanist install"
    This reverts commit 36123d022ef4aaf7cf1e999e0a03a6944469e5a7.

--- arcanist/src/lint/linter/__tests__/ArcanistHLintLinterTestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-final class ArcanistHLintLinterTestCase extends ArcanistArcanistLinterTestCase {
-  public function testHlintLinter() {
-    $this->executeTestsInDirectory(
-      dirname(__FILE__).'/hlint/',
-      new ArcanistHLintLinter());
-  }
-}
