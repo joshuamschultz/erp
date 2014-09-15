5bb7d24f0ce9e131fa3e98eb359ab05da97c3d2d
Joshua Schultz <joshuamschultz@gmail.com>
2014-09-15 15:58:56 +0530

    Fixed Create Receivable bug

--- app/models/receivable_line.rb
+++ app/models/receivable_line.rb
@@ -14,12 +14,12 @@ class ReceivableLine < ActiveRecord::Base
 
 	def update_receivable_total
 		receivable_total = self.receivable.process_receivable_total		
-		self.update_gl_account
+		# self.update_gl_account
     end
-    def update_gl_account
-    	CommonActions.update_gl_accounts('INVENTORY', 'decrement',self.receivable_line_cost)
-    	CommonActions.update_gl_accounts('RECEIVBALE EMPLOYEES', 'increment',self.receivable_line_cost )	
-    end
+    # def update_gl_account
+    # 	CommonActions.update_gl_accounts('INVENTORY', 'decrement',self.receivable_line_cost)
+    # 	CommonActions.update_gl_accounts('RECEIVBALE EMPLOYEES', 'increment',self.receivable_line_cost )	
+    # end
     
    
 end
