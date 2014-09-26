40df6ad52400d33141cd4ff0e358214b60f006a3
Joshua Schultz <joshuamschultz@gmail.com>
2014-09-26 10:20:14 +0530

    Modfication for Allocate Amount to Amounts: Receivable

--- app/models/payable_account.rb
+++ app/models/payable_account.rb
@@ -14,6 +14,8 @@ class PayableAccount < ActiveRecord::Base
   before_destroy :process_before_destory
   
   def process_after_save
+    @gl_account_to_update = GlAccount.where(:gl_account_identifier=> "21010").first
+    @gl_account_id_to_update = @gl_account_to_update.id
   	if self.gl_entry_id
   		@gl_entry = GlEntry.where(:id => self.gl_entry_id, :gl_account_id => self.gl_account_id).first       
       @gl_entry.update_attributes(:gl_entry_credit => self.payable_account_amount)        
@@ -24,9 +26,9 @@ class PayableAccount < ActiveRecord::Base
   	end
   	payable = Payable.where(:id => self.payable_id).first
   	pacctsum =  payable.payable_accounts.sum(:payable_account_amount).to_f  	
-  	@gl_entry = GlEntry.where(:payable_id => self.payable_id, :gl_account_id => 24).first
+  	@gl_entry = GlEntry.where(:payable_id => self.payable_id, :gl_account_id => @gl_account_id_to_update).first
   	if @gl_entry.blank?
-  		@gl_entry = GlEntry.new(:gl_account_id => 24, :gl_entry_description => "Transaction", :gl_entry_credit => pacctsum , :gl_entry_active => 1, :gl_entry_date => Date.today.to_s, :payable_account_id => self.id, :payable_id => self.payable_id)  		
+  		@gl_entry = GlEntry.new(:gl_account_id => @gl_account_id_to_update, :gl_entry_description => "Transaction", :gl_entry_credit => pacctsum , :gl_entry_active => 1, :gl_entry_date => Date.today.to_s, :payable_account_id => self.id, :payable_id => self.payable_id)  		
       @gl_entry.save   	
     else
     	@gl_entry.update_attributes(:gl_entry_credit => pacctsum )
@@ -34,7 +36,7 @@ class PayableAccount < ActiveRecord::Base
   	@gl_account = GlAccount.where(:id => self.gl_account_id).first
     amount = @gl_account.gl_account_amount - self.payable_account_amount_was.to_f + self.payable_account_amount.to_f
     @gl_account.update_attributes(:gl_account_amount => amount)
-    @gl_account = GlAccount.where(:id => 24).first
+    @gl_account = GlAccount.where(:id => @gl_account_id_to_update).first
     amount = @gl_account.gl_account_amount - self.payable_account_amount_was.to_f + self.payable_account_amount.to_f
     @gl_account.update_attributes(:gl_account_amount => amount)   
   end
