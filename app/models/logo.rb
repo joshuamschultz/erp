2f07a393db474e210f77351a2c3407fcdde6b61e
Joshua Schultz <joshuamschultz@gmail.com>
2014-10-04 22:11:10 +0530

    changes in q_doc

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -21,8 +21,12 @@ class Logo < ActiveRecord::Base
   		if self.jointable_type =="QualityDocument"
 			# self.joint_file_name=new_quote_identifier+"."+(self.joint_file_name).split('.')[-1]
 			self.joint_file_name=new_quote_identifier+"."+(self.joint_file_name).match(/(.*)\.(.*)/)[1..2][1]
-			unless MasterType.find_by_type_value(self.joint_file_name)
-				MasterType.create(type_name: self.joint_file_name, type_description: "", type_value: self.joint_file_name, type_category: "customer_quality_level", type_active: true)
+
+			unless MasterType.find_by_quality_document_id(self.jointable_id)
+				MasterType.create(type_name: self.joint_file_name, type_description: "", type_value: self.joint_file_name, type_category: "customer_quality_level", type_active: true, quality_document_id: self.jointable_id)
+			else
+			@master_type = MasterType.find_by_quality_document_id(self.jointable_id)
+			@master_type.update_attributes(:type_name => self.joint_file_name, :type_value => self.joint_file_name)
 			end
   		end
   	end
