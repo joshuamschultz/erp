58655ba00073a5c716955bae95b0afe27fd89ff4
manager <mohanraj@agileblaze.com>
2014-10-14 19:07:22 +0530

    Inventory_adjustment

---
+++ app/controllers/inventory_adjustments_controller.rb
@@ -0,0 +1,96 @@
+class InventoryAdjustmentsController < ApplicationController
+  before_filter :set_page_info
+  before_filter :set_autocomplete_values, only: [:create, :update]
+
+  def set_page_info
+      @menus[:inventory][:active] = "active"
+      simple_form_validation = true
+  end
+  def set_autocomplete_values
+
+    params[:inventory_adjustment][:item_alt_name_id], params[:alt_name_id] = params[:alt_name_id], params[:inventory_adjustment][:item_alt_name_id]
+    params[:inventory_adjustment][:item_alt_name_id] = params[:org_alt_name_id] if params[:inventory_adjustment][:item_alt_name_id] == "" || params[:inventory_adjustment][:item_alt_name_id].nil?
+
+  end
+  # GET /inventory_adjustments
+  # GET /inventory_adjustments.json
+  def index
+    @inventory_adjustments = InventoryAdjustment.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @inventory_adjustments }
+    end
+  end
+
+  # GET /inventory_adjustments/1
+  # GET /inventory_adjustments/1.json
+  def show
+    @inventory_adjustment = InventoryAdjustment.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @inventory_adjustment }
+    end
+  end
+
+  # GET /inventory_adjustments/new
+  # GET /inventory_adjustments/new.json
+  def new
+    @inventory_adjustment = InventoryAdjustment.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @inventory_adjustment }
+    end
+  end
+
+  # GET /inventory_adjustments/1/edit
+  def edit
+    @inventory_adjustment = InventoryAdjustment.find(params[:id])
+  end
+
+  # POST /inventory_adjustments
+  # POST /inventory_adjustments.json
+  def create
+    @inventory_adjustment = InventoryAdjustment.new(params[:inventory_adjustment])
+
+    respond_to do |format|
+      if @inventory_adjustment.save
+        format.html { redirect_to @inventory_adjustment, notice: 'Inventory adjustment was successfully created.' }
+        format.json { render json: @inventory_adjustment, status: :created, location: @inventory_adjustment }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @inventory_adjustment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /inventory_adjustments/1
+  # PUT /inventory_adjustments/1.json
+  def update
+    @inventory_adjustment = InventoryAdjustment.find(params[:id])
+
+    respond_to do |format|
+      if @inventory_adjustment.update_attributes(params[:inventory_adjustment])
+        format.html { redirect_to @inventory_adjustment, notice: 'Inventory adjustment was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @inventory_adjustment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /inventory_adjustments/1
+  # DELETE /inventory_adjustments/1.json
+  def destroy
+    @inventory_adjustment = InventoryAdjustment.find(params[:id])
+    @inventory_adjustment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to inventory_adjustments_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ app/models/inventory_adjustment.rb
@@ -0,0 +1,18 @@
+class InventoryAdjustment < ActiveRecord::Base
+  attr_accessible :inventory_adjustment_description, :inventory_adjustment_quantity, :item_alt_name_id, :item_id, :quality_lot_id
+
+
+  belongs_to :item
+  belongs_to :item_alt_name
+
+  before_save :update_item
+  after_save :update_item
+  def update_item
+    self.item_id = item_no
+  end
+  def item_no
+  	self.item_alt_name.item.id
+  end
+
+
+end
