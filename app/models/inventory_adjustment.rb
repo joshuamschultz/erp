bef65b2966f737b5ec787f34d394d2f5ddddb2ad
Joshua Schultz <joshuamschultz@gmail.com>
2014-10-16 19:09:54 +0530

    changes in inventoryadjust

--- app/models/inventory_adjustment.rb
+++ app/models/inventory_adjustment.rb
@@ -7,13 +7,13 @@ class InventoryAdjustment < ActiveRecord::Base
   belongs_to :quality_lot
 
 
-  before_create :before_create_process
+  after_save :before_create_process
   def before_create_process
     self.item_id = item_no
 
     @quality_lot = QualityLot.find(self.quality_lot_id)
     @quality_lot.update_attribute(:lot_quantity , self.inventory_adjustment_quantity)
-    
+
   end
   def item_no
   	self.item_alt_name.item.id
