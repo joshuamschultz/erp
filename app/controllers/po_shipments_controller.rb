c25c5b74b32f07308e49c4f83680ec385a9e43a0
manager <mohanraj@agileblaze.com>
2014-11-14 15:30:04 +0530

    changes in po_shipment

--- app/controllers/po_shipments_controller.rb
+++ app/controllers/po_shipments_controller.rb
@@ -31,6 +31,8 @@ class PoShipmentsController < ApplicationController
                 po_shipment[:po_shipped_date] = po_shipment.created_at.strftime("%Y-%m-%d at %I:%M %p")
                 po_shipment[:links] = params[:type] == "history" ? "" : CommonActions.object_crud_paths(nil, edit_po_shipment_path(po_shipment), nil)
                 po_shipment[:item_part_no] = (params[:create_payable].present? ? po_shipment.payable_checkbox(params[:type]) : "") + po_shipment[:item_part_no]
+                po_shipment[:lot] =po_shipment.quality_lot.lot_control_no.split('-')[0]+"-"+"<a href='/quality_lots/#{quality_lot.id}'>#{quality_lot.lot_control_no.split('-')[1]}</a>"
+   
                 i += 1
             }
             render json: {:aaData => @po_shipments}
