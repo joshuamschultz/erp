ff3f53ba4ec8f854762d6f17943ddb2c46a4cecc
manager <mohanraj@agileblaze.com>
2014-12-02 17:57:48 +0530

    email_issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -31,13 +31,13 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user, params = {})
       @user = user
-      to_address = (ENV['RAILS_ENV'] == "development") ? "sreejeshkp@agileblaze.com" : ["sreejeshkp@agileblaze.com", "joshuamschultz@gmail.com"]
+      to_address = (ENV['RAILS_ENV'] == "development") ? "mohan.ms1998@gmail.com" : ["sreejeshkp@agileblaze.com", "joshuamschultz@gmail.com"]
 
       # "sreejeshkp@agileblaze.com"
 
       # ["sreejeshkp@agileblaze.com", "joshuamschultz@gmail.com"]
         
-        mail(:to => to_address, :subject => "Welcome to Alliance Fastners").deliver
+        mail(:to => @user.email, :subject => "Welcome to Alliance Fastners").deliver
         puts "Mail Send!"
   end
 
