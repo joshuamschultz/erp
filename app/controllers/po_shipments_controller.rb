6b18606ce81b523f4d45854a3142219ab63e58de
Joshua Schultz <joshuamschultz@gmail.com>
2014-12-19 16:53:12 +0530

    changes in po_shipment

--- app/controllers/po_shipments_controller.rb
+++ app/controllers/po_shipments_controller.rb
@@ -33,12 +33,21 @@ class PoShipmentsController < ApplicationController
 
                 # po_shipment = so_line_data_list(po_shipment, true)  
                 po_shipment[:po_shipped_date] = po_shipment.created_at.strftime("%Y-%m-%d at %I:%M %p")
-                po_shipment[:links] = params[:type] == "history" ? "" : CommonActions.object_crud_paths(nil, edit_po_shipment_path(po_shipment), nil)
+                if can? :edit, po_shipment
+                  po_shipment[:links] = params[:type] == "history" ? "" : CommonActions.object_crud_paths(nil, edit_po_shipment_path(po_shipment), nil)
+                else
+                  po_shipment[:links] = params[:type] == "history" ? "" : CommonActions.object_crud_paths(nil, nil, nil)
+                end
                 po_shipment[:item_part_no] = (params[:create_payable].present? ? po_shipment.payable_checkbox(params[:type]) : "") + po_shipment[:item_part_no]
                 if po_shipment
                   po_header_id = po_shipment.po_line.po_header
                   quality_lot = QualityLot.find_by_po_header_id(po_header_id) 
-                  po_shipment[:lot] =  quality_lot.present? ? "<a href='/quality_lots/#{quality_lot.id}'>#{quality_lot.lot_control_no.split('-')[1]}</a>"  : ""
+                if can? :edit, po_shipment
+
+                  po_shipment[:lot] =  quality_lot.present? ? "<a href='/quality_lots/#{quality_lot.id}'>#{quality_lot.lot_control_no.split('-')[1]}</a>"  : "" 
+                else
+                  po_shipment[:lot]= ""
+                end
                 else
                    po_shipment[:lot]= ""
                 end
