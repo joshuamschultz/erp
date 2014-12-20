b0dc2e272625259494ce302c66de7094f7863254
Rehna Thomas <rehna@manager-G31D-M7.(none)>
2014-12-20 10:36:07 +0530

    To stay the menu for reconcile

--- app/controllers/reconciles_controller.rb
+++ app/controllers/reconciles_controller.rb
@@ -1,9 +1,11 @@
 class ReconcilesController < ApplicationController
   before_filter :find_reconciles, only: [:index] 
-
+   before_filter :set_page_info
   # GET /reconciles
   # GET /reconciles.json
-
+  def set_page_info
+      @menus[:general_ledger][:active] = "active"
+  end
 
   def find_reconciles
       params[:reconcile_type] ||= nil
