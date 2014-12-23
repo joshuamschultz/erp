9439d1ad418847093e89ee436521ee0d9e1138b3
Joshua Schultz <joshuamschultz@gmail.com>
2014-12-23 20:32:09 +0530

    packing_slip

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module AllianceFasteners
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile += ['bootstrap.min.js', 'app_theme.js', "main.js", "main.css", "app_validation.js", "jr100.css"]
+    config.assets.precompile += ['bootstrap.min.js', 'app_theme.js', "main.js", "main.css", "app_validation.js","pk100.css", "jr100.css"]
 
     config.middleware.use PDFKit::Middleware, :print_media_type => true
     
@@ -69,7 +69,6 @@ module AllianceFasteners
     # In `config/environments/development.rb`
     #  config.action_controller.asset_host = "http://localhost:3001"
     
-
     # # In `config/environments/production.rb`
     # config.action_controller.asset_host = "http://erp.chessgroupinc.com"
   end
