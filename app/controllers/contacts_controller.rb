3a90dd3d4e6f5ead83f28b80e6813c34c985e029
Joshua Schultz <joshuamschultz@gmail.com>
2015-01-31 21:09:09 +0530

    po, quote, organization, item

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,7 +21,9 @@ class ContactsController < ApplicationController
   end
 
   def set_page_info
+    unless user_signed_in? && ( current_user.is_vendor? || current_user.is_customer? )
       @menus[:contacts][:active] = "active"
+    end
   end
 
 
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -19,7 +19,9 @@ class OrganizationsController < ApplicationController
   end
 
   def set_page_info
+    unless user_signed_in? && ( current_user.is_vendor? || current_user.is_customer? )
       @menus[:contacts][:active] = "active"
+    end
   end
 
   def get_autocomplete_items(parameters)
