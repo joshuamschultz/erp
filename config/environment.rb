8ebebcafc12595f0763e31ab176ad2b90b1a1192
Joshua Schultz <joshuamschultz@gmail.com>
2015-03-05 16:07:34 +0530

    config

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,6 @@ require File.expand_path('../application', __FILE__)
 AllianceFasteners::Application.initialize!
 
 Mime::Type.register 'application/pdf', :pdf
+
+config.logger = Logger.new(STDOUT)
+config.logger = Log4r::Logger.new("Application Log")
