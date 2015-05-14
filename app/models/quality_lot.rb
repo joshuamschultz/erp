13e3d7e39ea127cfb4ff2b8cfdd7a09e9305a07d
Joshua Schultz <joshuamschultz@gmail.com>
2015-05-14 19:52:48 +0530

    Alt item names

--- app/models/quality_lot.rb
+++ app/models/quality_lot.rb
@@ -133,6 +133,7 @@ class QualityLot < ActiveRecord::Base
 		else
 			current_count =current_count+1
 		end
+		Item.skip_callback("update", :after, :update_alt_name)
 		self.po_line.item.update_attribute(:lot_count , current_count)
 
 
