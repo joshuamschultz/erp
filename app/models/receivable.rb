b11afed19c8989ca5c5528acf702c19c982e75ee
manager <mohanraj@agileblaze.com>
2015-05-28 20:53:14 +0530

    changes in invoice report

--- app/models/receivable.rb
+++ app/models/receivable.rb
@@ -291,6 +291,7 @@ class Receivable < ActiveRecord::Base
     end 
 
     i = 1
+    j = 0
     flag = 1
     flag2 = 1
     sub_total = 0.0
@@ -336,12 +337,13 @@ class Receivable < ActiveRecord::Base
           content += '</table></article>'
           content += '<article ><div class="footer"><div class="page"><h3>Page</h3><span>'+j.to_s+' </span></div><div class="original"><table width="250" border="0" cellpadding="0" cellspacing="0"><tr><th width="169" align="right" scope="row">SUB TOTAL :</th><td width="131" align="right">'+sub_total.to_s+'</td></tr><tr><th align="right" scope="row">FREIGHT :</th><td align="right">'+self.receivable_freight.to_s+'</td></tr><tr><th align="right" scope="row">TOTAL :</th><td align="right">'+s_sub_total.to_s+'  </td></tr></table></div></div></article> </section>'
           content += ' <div style="page-break-after:always;">&nbsp; </div>'  
-          j+=1
+      
         end
 
         if len == index+1 && i != 10 
           # j = 1
           # j+=1
+          j+=1
           content += '</table></article>'
           content += '<article ><div class="footer"><div class="page"><h3>Page</h3><span>  haihais'+j.to_s+'</span></div><div class="original"><table width="250" border="0" cellpadding="0" cellspacing="0"><tr><th width="169" align="right" scope="row">SUB TOTAL :</th><td width="131" align="right">'+sub_total.to_s+'</td></tr><tr><th align="right" scope="row">FREIGHT :</th><td align="right">'+self.receivable_freight.to_s+'</td></tr><tr><th align="right" scope="row">TOTAL :</th><td align="right">'+s_sub_total.to_s+'  </td></tr></table></div></div></article> </section>'
           content += ' <div style="page-break-after:always;">&nbsp; </div>'  
