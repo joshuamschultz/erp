f5163b9adc621311f8df2e9b9bac5565248ea9e6
Joshua Schultz <joshuamschultz@gmail.com>
2015-06-05 10:36:04 +0530

    master module

--- app/modules/common_actions.rb
+++ app/modules/common_actions.rb
@@ -491,8 +491,8 @@ module CommonActions
       elsif model_type == "ProcessType"
      		common_process_model(model_type,model_id,quality_user)
 
-     elsif model_type == "PoLine"
-     		if model_id.organization.present? && model_id.organization.min_vendor_quality.quality_name.ord <= model_id.po_header.organization.vendor_quality.quality_name.ord
+     	elsif model_type == "PoLine"
+     		if model_id.organization.min_vendor_quality.quality_name.ord <= model_id.po_header.organization.vendor_quality.quality_name.ord
      			common_process_model(model_type,model_id,quality_user)
      		end
       end
