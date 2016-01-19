396b713ef62ac57cf5495fdc28c3fcd44e5eb966
Joshua Schultz <joshuamschultz@gmail.com>
2016-01-19 09:59:30 +0530

    Revsions are wrong

--- app/models/item.rb
+++ app/models/item.rb
@@ -98,9 +98,17 @@ class Item < ActiveRecord::Base
       SoHeader.joins(:so_lines).where("so_lines.item_id = ?", self.id).order('created_at desc')
   end 
 
-  def qty_on_order
+  def qty_on_order(revision)
     # self.po_lines.sum(:po_line_quantity)
     # self.last.po_lines.joins(:po_header).where(po_headers: {po_status: "open"}).sum(:po_line_quantity)
+    item_revision = ItemRevision.find(revision)
+    item_revision.po_lines.where(:po_line_status => "open").includes(:po_header).where(po_headers: {po_status: "open"}).sum("po_line_quantity - po_line_shipped")
+
+  end
+
+  def qty_on_order_item
+    # self.po_lines.sum(:po_line_quantity)
+ 
     self.po_lines.where(:po_line_status => "open").includes(:po_header).where(po_headers: {po_status: "open"}).sum("po_line_quantity - po_line_shipped")
 
   end
@@ -136,10 +144,89 @@ class Item < ActiveRecord::Base
 
   # def item_sell_price
   # end
-
   def current_location
       po_shipment = self.po_shipments.order(:created_at).last
       po_shipment.nil? ? "-" : po_shipment.po_shipped_unit.to_s + " - " + po_shipment.po_shipped_shelf
   end
+
+  def stock(item_revision)
+     item_revision.quality_lots.sum(:quantity_on_hand) if item_revision.quality_lots.present?
+  end
+
+  def time_stock(item_revision)
+    (item_revision.item_revision_weekly_usage != nil && item_revision.item_revision_weekly_usage != 0) ? item_revision.item.stock(item_revision) / item_revision.item_revision_weekly_usage.to_f : 0
+  end
   
+  def run_out(item_revision)
+    (item_revision.item_revision_lead_time != nil && item_revision.item_revision_lead_time != 0) ?  item_revision.item.time_stock(item_revision).to_f / item_revision.item_revision_lead_time.to_f : 0
+  end
+
+  def usage(item_revision)
+     item_revision.so_lines.includes(:so_shipments).where("so_shipments.so_shipped_status =? AND so_shipments.created_at >= ? AND so_shipments.created_at >= ? ",'shipped',Date.today, Date.today - 12.month).sum("so_shipments.so_shipped_count").to_f
+  end
+
+  def a_time_stock(item_revision)
+    (item_revision.item.usage(item_revision) !=0 && item_revision.item.stock(item_revision) !=0) ? item_revision.item.stock(item_revision).to_f / (item_revision.item.usage(item_revision).to_f / 52).to_f : 0
+  end
+
+  def a_run_out(item_revision)
+    (item_revision.item_revision_lead_time !=0 && item_revision.item_revision_lead_time != nil ) ? item_revision.item.a_time_stock(item_revision).to_f / item_revision.item_revision_lead_time.to_f : 0
+  end
+
+  def absolute_value(item_revision)
+    stock(item_revision).to_f + qty_on_order(item_revision).to_f + qty_on_committed(item_revision).to_f
+  end
+
+  def self.inventory_report_data
+    items =  Item.order('item_part_no asc')
+    len = items.length
+    i,j,flag = 1,1,1
+
+    content = '' 
+    items.select{|item|
+      item.item_revisions.each_with_index do |item_revision, index|
+
+        if item_revision.item.absolute_value(item_revision) > 0
+
+          temp = '<tr><td align="center" valign="middle">'+item_revision.item.item_part_no+'</td><td align="center" valign="middle">'+item_revision.item_revision_name+'</td><td align="center" valign="middle">'+item_revision.item.stock(item_revision).to_s+'</td><td align="center" valign="middle">'+item_revision.item.qty_on_order(item_revision).to_s+'</td><td align="center" valign="middle">'+item_revision.item.qty_on_committed(item_revision).to_s+'</td><td align="center" valign="middle">'+item_revision.item.time_stock(item_revision).to_s+'</td><td align="center" valign="middle">'+(item_revision.item.run_out(item_revision).to_f - 1) .to_s+'</td><td align="center" valign="middle">'+item_revision.item.usage(item_revision).to_s+'</td><td align="center" valign="middle">'+item_revision.item.a_time_stock(item_revision).to_s+'</td><td align="center" valign="middle">'+item_revision.item.a_run_out(item_revision).to_s+'</td></tr>'
+
+          if flag ==1
+            content = '<div class="wrapper"><table width="100%" border="0" cellspacing="0" cellpadding="0"><tr align="left" valign="top"><td align="center"><img class="logo" alt=Smallest  src=http://erp.chessgroupinc.com/'+CompanyInfo.first.image.image.url(:original)+' /></td></tr></table></div>'
+            flag = 0
+          end
+          
+          if i == 1
+            content += '<div id="main-wrapper">'
+            content += '<div class="wrapper-1"><table width="100%" border="0" cellspacing="0" cellpadding="0"><tbody><tr><th align="center" valign="middle">Part No</th><th align="center" valign="middle">Revision</th><th align="center" valign="middle">Stock</th><th align="center" valign="middle">In Process</th><th align="center" valign="middle">Committed</th><th align="center" valign="middle">Time Stock</th><th align="center" valign="middle">Run Out</th><th align="center" valign="middle">Usage</th><th align="center" valign="middle">TS</th><th align="center" valign="middle">RO</th></tr>'
+          end
+
+          if j <= 20
+            content += temp
+          end
+
+          j+=1
+
+          if i==20
+            content += '</tbody></table></div></div>'
+            content +='<div style="page-break-after: always; "> &nbsp;  </div>'  
+          end
+
+          if len == index+1
+          end
+
+          i+=1
+
+          if i==21 
+            i= 1
+            j = 1
+            content 
+          end
+        end
+      end
+    }
+      content
+      html = '<!DOCTYPE html><title>Inventory_Report</title><!--[if lt IE 9]><script src="html5.js"></script><![endif]--><style type="text/css">@charset "utf-8";body {font-family: Arial, Helvetica, sans-serif;font-size: 12px;background-color: #FFF;margin-left: 0px;margin-top: 0px;margin-right: 0px;margin-bottom: 0px;}/* New Style */.clear {clear: both;}#main-wrapper {float: left;height: auto;width: 640px;border:1px solid #000;}table {border-collapse: collapse;}.logo {width: 180px;}.wrapper-1 td {border: 1px solid #000;padding: 6px;}.wrapper-1 th {border: 1px solid #000;font-size: 12px;padding: 6px;} .wrapper {width: 640px;</style>
+            '+content+'
+        '
+  end
 end
