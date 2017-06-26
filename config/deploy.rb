d3176033a124a3b07cb5bde4b4f0cc39ffb084a8
Joshua <me@joshuaschultz.com>
2017-06-26 07:02:31 -0400

    cap setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,13 @@ set :repo_url, "git@github.com:JoshuaMSchultz/erp-version-2.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/chess_erp'
+set :application,     'chess_erp'
+set :user,            'deploy'
+
+# Settings
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -36,3 +42,5 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+#
+set :rails_env, 'production'
