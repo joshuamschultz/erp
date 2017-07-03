4904a89bc57b4cee44f0e056a15e9086b0e11eba
Joshua Schultz <joshuamschultz@gmail.com>
2017-07-03 13:32:21 -0400

    bundle intall

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.1'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += ['less.min.js', 'commoncls.js', 'app_validation.js', 'jr100.css']
+Rails.application.config.assets.precompile += ['less.min.js', 'commoncls.js', 'app_validation.js', 'jr100.css',*.png *.jpg *.jpeg *.gif *.woff *.ttf *.eot *.svg]
