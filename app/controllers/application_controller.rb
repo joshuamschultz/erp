246b3c01d41cd8335706b2a5c78050617b5ab1a0
Joshua <me@joshuaschultz.com>
2017-07-07 22:53:50 -0400

    cleaned up model and controller for corporate info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,23 @@ class ApplicationController < ActionController::Base
   include CommonActions
   protect_from_forgery
 
+  respond_to :html, :json
+
   before_action :set_locale
   before_action :determine_website
   before_action :authenticate_user!
   before_action :initialize_request
-
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.permissions_error_url, :alert => exception.message
-  end
-
-
   before_action :set_current_user
   around_action :set_time_zone
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.permissions_error_url, alert: exception.message
+  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
   def set_time_zone(&block)
     if current_user.present?
       Time.use_zone(current_user.time_zone, &block)
@@ -30,12 +31,13 @@ class ApplicationController < ActionController::Base
     User.current_user = current_user
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     CommonActions.clear_temp_objects
     root_path
   end
+
   def determine_website
-   $sitename = request.host_with_port
-   ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+    $sitename = request.host_with_port
+    ActionMailer::Base.default_url_options = { host: request.host_with_port }
   end
 end
