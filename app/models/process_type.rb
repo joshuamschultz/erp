0668c4477359549cd7d6e1b1bfd76d235a181190
Joshua <me@joshuaschultz.com>
2017-08-13 23:34:48 -0500

    fixed the spec Model and controller

--- app/models/process_type.rb
+++ app/models/process_type.rb
@@ -14,89 +14,75 @@
 class ProcessType < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
+  has_many :organization_processes, dependent: :destroy
+  has_many :organizations, through: :organization_processes
+  has_many :item_processes, dependent: :destroy
+  has_many :item_revisions, through: :item_processes
+  has_many :specifications, through: :process_type_specifications
+  has_many :process_type_specifications, dependent: :destroy
+  has_many :specifications, through: :process_type_specifications
+
+  has_one :attachment, as: :attachable, dependent: :destroy
+  accepts_nested_attributes_for :attachment, allow_destroy: true
+
+  has_one :notification, as: :notable, dependent: :destroy
+  accepts_nested_attributes_for :notification, allow_destroy: true
 
   after_initialize :default_values
-
-  def default_values
-    self.process_active = true if self.process_active.nil?
-  end
+  before_save :before_save_values
 
   # (validates_uniqueness_of :process_short_name if validates_length_of :process_short_name, :minimum => 2, :maximum => 20) if validates_presence_of :process_short_name
-
   # validates_length_of :process_description, :maximum => 50
-
-  has_many :organization_processes, :dependent => :destroy
-  has_many :organizations, :through => :organization_processes
-  has_many :item_processes, :dependent => :destroy
-  has_many :item_revisions, :through => :item_processes
-  has_many :specifications, :through => :process_type_specifications
-
-  has_many :process_type_specifications, :dependent => :destroy
-  has_many :specifications, :through => :process_type_specifications
-
-  has_one :attachment, :as => :attachable, :dependent => :destroy
-
 
   default_scope { order('process_short_name ASC') }
 
-  accepts_nested_attributes_for :attachment, :allow_destroy => true
-
-
-  has_one :notification, :as => :notable,  dependent: :destroy
-
-  accepts_nested_attributes_for :notification, :allow_destroy => true
-
-
+  def default_values
+    self.process_active = true if process_active.nil?
+  end
 
   def self.process_item_associations(process_type, params)
-        if process_type
-          p params[:specs]
-          specs = params[:specs] || []
-          process_type.process_type_specifications.destroy_all
-          if specs
-              specs.each do |specification_id|
-                unless process_type.process_type_specifications.find_by_specification_id(specification_id)
-                    process_type.process_type_specifications.new(:specification_id => specification_id).save
-                end
-              end
-          end
-        end
-  end
-
-  def redirect_path
-      process_type_path(self)
-  end
-
-  before_save :before_save_values
-
-  def before_save_values
-      self.process_short_name = self.attachment.attachment_name
-  end
-  def self.item_process_type(item)
-      process_types=[]
-     Item.find(item).item_revisions.each do |item_revision|
-        if item_revision.present?
-          item_revision.item_processes.each do |process|
-            if process.present?
-              process_types<<process.process_type
-            end
+    if process_type
+      p params[:specs]
+      specs = params[:specs] || []
+      process_type.process_type_specifications.destroy_all
+      if specs
+        specs.each do |specification_id|
+          unless process_type.process_type_specifications.find_by_specification_id(specification_id)
+            process_type.process_type_specifications.new(specification_id: specification_id).save
           end
         end
       end
-      process_types = process_types.uniq
-      return process_types
+    end
+  end
+
+  def redirect_path
+    process_type_path(self)
+  end
+
+  def before_save_values
+    self.process_short_name = attachment.attachment_name
+  end
+
+  def self.item_process_type(item)
+    process_types = []
+    Item.find(item).item_revisions.each do |item_revision|
+      next unless item_revision.present?
+      item_revision.item_processes.each do |process|
+        process_types << process.process_type if process.present?
+      end
+    end
+    process_types = process_types.uniq
+    process_types
   end
 
   def self.process_type_specifications(process_types)
     specifications = []
     process_types.each do |process_type|
-      if process_type.present?
-         process_type.specifications.each do |pro_spec|
-          specifications<<pro_spec
-        end
+      next unless process_type.present?
+      process_type.specifications.each do |pro_spec|
+        specifications << pro_spec
       end
     end
     specifications.uniq
   end
-
 end
--- app/models/specification.rb
+++ app/models/specification.rb
@@ -14,63 +14,64 @@
 class Specification < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
-  after_initialize :default_values
+  has_many :item_specifications, dependent: :destroy
+  has_many :item_revisions, through: :item_specifications
+  has_many :process_type_specifications, dependent: :destroy
+  has_many :process_types, through: :process_type_specifications
 
-  def default_values
-    self.specification_active = true if self.specification_active.nil?
-  end
+  has_one :attachment, as: :attachable, dependent: :destroy
+  accepts_nested_attributes_for :attachment, allow_destroy: true
 
-  
-
-  # (validates_uniqueness_of :specification_identifier if validates_length_of :specification_identifier, :minimum => 2, :maximum => 50) if validates_presence_of :specification_identifier
-
-  # validates_length_of :specification_description, :maximum => 50
-
-  has_many :item_specifications, :dependent => :destroy
-  has_many :item_revisions, :through => :item_specifications
-
-  has_many :process_type_specifications, :dependent => :destroy
-  has_many :process_types, :through => :process_type_specifications
-
-  has_one :attachment, :as => :attachable, :dependent => :destroy
-
-
-  has_one :notification, :as => :notable,  dependent: :destroy
-
-  accepts_nested_attributes_for :notification, :allow_destroy => true
-
-  
-  
-  default_scope { order('specification_identifier ASC') } 
-
-  accepts_nested_attributes_for :attachment, :allow_destroy => true
-
-  def redirect_path
-      specification_path(self)
-  end
+  has_one :notification, as: :notable, dependent: :destroy
+  accepts_nested_attributes_for :notification, allow_destroy: true
 
   before_save :before_save_values
+  after_initialize :default_values
+
+  # (validates_uniqueness_of :specification_identifier if validates_length_of :specification_identifier, :minimum => 2, :maximum => 50) if validates_presence_of :specification_identifier
+  # validates_length_of :specification_description, :maximum => 50
+
+  default_scope { order('specification_identifier ASC') }
+
+  def redirect_path
+    specification_path(self)
+  end
+
+  def default_values
+    self.specification_active = true if specification_active.nil?
+  end
 
   def before_save_values
-      self.specification_identifier = self.attachment.attachment_name
+    self.specification_identifier = attachment.attachment_name
   end
+
   def self.item_specification(item)
-    specifications=[]
+    # create an array called specifications
+    specifications = []
+    # Create a loop for each revision
     Item.find(item).item_revisions.each do |item_revision|
-      if item_revision.present?
-        item_revision.item_specifications.each do |process|
-          if process.present?
-            specifications<<process.specification
-          end
-        end
+      next unless item_revision.present?
+      # Grab all the specs for that revision
+      # and load them into the specifications array
+      item_revision.item_specifications.each do |process|
+        specifications << process.specification if process.present?
       end
     end
-    process_types =ProcessType.item_process_type(item)
+    # get all processes with a a part
+    process_types = ProcessType.item_process_type(item)
+    # grab all specs with the process and create a loop
+    # Add those to the specifications array as well.
     process_specifications = ProcessType.process_type_specifications(process_types)
     process_specifications.each do |process_spec|
       specifications << process_spec
     end
-
+    # get rid of duplicates.
     specifications.uniq
-  end     
+  end
+  # TODO: if this what I think it is, to assign specs to new parts or something.
+  # We need to change
+  # Not all specs go to the part. Some go to the part, and but those that come from
+  # a process need to have that same process associated. Ex, not a black oxide spec
+  # to a part. The part should get that spec when we assign the b/o process.
+  # Check what the spec array is used for.
 end
