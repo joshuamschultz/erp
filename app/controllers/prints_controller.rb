1c54ea4d16f2c664ad934bbfe8e7d32a5ec14996
Joshua <me@joshuaschultz.com>
2017-09-15 13:34:47 -0400

    fix prints MVC

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,19 +1,9 @@
 class PrintsController < ApplicationController
   before_action :set_page_info
-
-  autocomplete :print, :print_identifier, :full => true
+  before_action :set_print, only: %i[show edit update destroy]
   before_action :user_permissions
 
-
-  def user_permissions
-    if  user_signed_in? && (current_user.is_vendor? || current_user.is_customer?)
-        authorize! :edit, Print
-    end
-  end
-
-  def set_page_info
-      @menus[:inventory][:active] = "active"
-  end
+  autocomplete :print, :print_identifier, full: true
 
   # GET /prints
   # GET /prints.json
@@ -22,36 +12,30 @@ class PrintsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      @prins = Array.new
-      format.json {
-        @prints = @prints.collect{ |print|
-          prind = Hash.new
+      @prins = []
+      format.json do
+        @prints = @prints.collect do |print|
+          prind = {}
           print.attributes.each do |key, value|
             prind[key] = value
           end
           attachment = print.attachment.attachment_fields
-          print.attachment.attachment_fields.each  do |key, value|
+          print.attachment.attachment_fields.each do |key, value|
             prind[key] = value
           end
           prind[:attachment_name] = CommonActions.linkable(print_path(print), attachment[:attachment_name])
           prind[:links] = CommonActions.object_crud_paths(nil, edit_print_path(print), nil)
           @prins.push(prind)
-        }
-        render json: {:aaData => @prins}
-      }
+        end
+        render json: { aaData: @prins }
+      end
     end
   end
 
   # GET /prints/1
   # GET /prints/1.json
   def show
-    @print = Print.find(params[:id])
     @attachment = @print.attachment
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @print }
-    end
   end
 
   # GET /prints/new
@@ -59,16 +43,10 @@ class PrintsController < ApplicationController
   def new
     @print = Print.new
     @print.build_attachment
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @print }
-    end
   end
 
   # GET /prints/1/edit
   def edit
-    @print = Print.find(params[:id])
     @attachment = @print.attachment
   end
 
@@ -76,57 +54,56 @@ class PrintsController < ApplicationController
   # POST /prints.json
   def create
     @print = Print.new(print_params)
-
-    respond_to do |format|
-      @print.attachment.created_by = current_user
-      if @print.save
-        CommonActions.notification_process("Print", @print)
-        format.html { redirect_to prints_path, notice: 'Print was successfully created.' }
-        format.json { render json: @print, status: :created, location: @print }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @print.errors, status: :unprocessable_entity }
-      end
+    @print.attachment.created_by = current_user
+    if @print.save
+      CommonActions.notification_process('Print', @print)
+      respond_with @print
+    else
+      respond_with @print
     end
   end
 
   # PUT /prints/1
   # PUT /prints/1.json
   def update
-    @print = Print.find(params[:id])
-
-    respond_to do |format|
-      @print.attachment.updated_by = current_user
-      if @print.update_attributes(print_params)
-        CommonActions.notification_process("Print", @print)
-        format.html { redirect_to prints_path, notice: 'Print was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @print.errors, status: :unprocessable_entity }
-      end
+    @print.attachment.updated_by = current_user
+    if @print.update_attributes(print_params)
+      CommonActions.notification_process('Print', @print)
+      respond_with @print
+    else
+      respond_with @print
     end
   end
 
   # DELETE /prints/1
   # DELETE /prints/1.json
   def destroy
-    @print = Print.find(params[:id])
     @print.destroy
+    respond_with :prints
+  end
 
-    respond_to do |format|
-      format.html { redirect_to prints_path, notice: 'Print was successfully deleted.' }
-      format.json { head :no_content }
+  def user_permissions
+    if user_signed_in? && (current_user.is_vendor? || current_user.is_customer?)
+      authorize! :edit, Print
     end
   end
+
+  def set_page_info
+    @menus[:inventory][:active] = 'active'
+  end
+
   private
 
-    def set_print
-      @print = Print.find(params[:id])
-    end
+  def set_print
+    @print = Print.find(params[:id])
+  end
 
-    def print_params
-      params.require(:print).permit(:print_active, :print_created_id, :print_description, :print_identifier,
-                                    :print_notes, :print_updated_id, :attachment_attributes, :notification_attributes)
-    end
+  def print_params
+    params.require(:print).permit(:print_active, :print_created_id, :print_description, :print_identifier,
+                                  :print_notes, :print_updated_id, attachment_attributes: %i[attachable_id attachable_type attachment_revision_title attachment_revision_date
+                                                                                             attachment_effective_date attachment_name attachment_description attachment_document_type
+                                                                                             attachment_document_type_id attachment_notes attachment_public attachment_active
+                                                                                             attachment_status attachment_status_id attachment_created_id attachment_updated_id attachment
+                                                                                             attachment_file_name], notification_attributes:  %i[notable_id notable_type note_status user_id])
+  end
 end
--- app/models/print.rb
+++ app/models/print.rb
@@ -16,39 +16,22 @@
 class Print < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
-  attr_accessible :print_active, :print_created_id, :print_description, :print_identifier, 
-  :print_notes, :print_updated_id, :attachment_attributes, :notification_attributes
-
-  
   # has_one :item_print, :dependent => :destroy
   # has_one :item_revision, :through => :item_print
   has_many :item_revisions
-  has_one :attachment, :as => :attachable, :dependent => :destroy
-
-
-  has_one :notification, :as => :notable,  dependent: :destroy
-
-  accepts_nested_attributes_for :notification, :allow_destroy => true
-
+  has_one :attachment, as: :attachable, dependent: :destroy
+  accepts_nested_attributes_for :attachment, allow_destroy: true
+  has_one :notification, as: :notable, dependent: :destroy
+  accepts_nested_attributes_for :notification, allow_destroy: true
 
   after_initialize :default_values
-
-  accepts_nested_attributes_for :attachment, :allow_destroy => true
-
-  def default_values
-    	self.print_active = true if self.attributes.has_key?("print_active") && self.print_active.nil?
-  end
-
-  # validates_length_of :print_identifier, :minimum => 2, :maximum => 50 if validates_presence_of :print_identifier
-
-  def redirect_path
-      print_path(self)
-  end
-
   before_save :before_save_values
 
-  def before_save_values
-      self.print_identifier = self.attachment.attachment_name
+  def default_values
+    self.print_active = true if attributes.key?('print_active') && print_active.nil?
   end
 
+  def before_save_values
+    self.print_identifier = attachment.attachment_name
+  end
 end
