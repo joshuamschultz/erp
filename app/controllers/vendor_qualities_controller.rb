8ddffea253a229ebee3aab38b8abf5c96e956032
Joshua <me@joshuaschultz.com>
2017-12-18 09:44:09 -0500

    vendor qualities done

--- app/controllers/vendor_qualities_controller.rb
+++ app/controllers/vendor_qualities_controller.rb
@@ -1,7 +1,54 @@
 class VendorQualitiesController < ApplicationController
+  before_action :set_vendor_quality, only: %i[show edit update destroy]
   before_action :set_page_info
   before_action :user_permissions
 
+  def index
+    @vendor_qualities = VendorQuality.all
+  end
+
+  def show
+    @attachable = @vendor_quality
+  end
+
+  def new
+    @duplicate = VendorQuality.find_by_id(params[:quality_id])
+    @vendor_quality = @duplicate.present? ? @duplicate.dup : VendorQuality.new
+  end
+
+  def edit
+  end
+
+  def create
+    @vendor_quality = VendorQuality.new(vendor_quality_params)
+    if @vendor_quality.save
+      respond_with @vendor_quality
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    if @vendor_quality.update_attributes(vendor_quality_params)
+      respond_with @vendor_quality
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @vendor_quality.destroy
+    redirect_to vendor_qualities_url
+  end
+
+  def set_default
+    @vendor_quality = VendorQuality.find(params[:vendor_quality_id])
+    if @vendor_quality
+        MasterType.where(:type_category => "default_vendor_quality").destroy_all
+        MasterType.create(:type_name => "Default Vendor Quality", :type_description => "", :type_value => @vendor_quality.id, :type_category => "default_vendor_quality", :type_active => true)
+    end
+    redirect_to @vendor_quality
+  end
 
   def user_permissions
     if  user_signed_in? && (current_user.is_vendor? || current_user.is_customer? )
@@ -13,114 +60,6 @@ class VendorQualitiesController < ApplicationController
       @menus[:quality][:active] = "active"
   end
 
-  # GET /vendor_qualities
-  # GET /vendor_qualities.json
-  def index
-    @vendor_qualities = VendorQuality.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      @vendor_qualitis = Array.new
-      format.json {
-          @vendor_qualities = @vendor_qualities.select{|quality|
-            qualiti = Hash.new
-          quality.attributes.each do |key, value|
-            qualiti[key] = value
-          end
-          qualiti[:links] = CommonActions.object_crud_paths(vendor_quality_path(quality), edit_vendor_quality_path(quality),
-                        vendor_quality_path(quality))
-          @vendor_qualitis.push(qualiti)
-        }
-
-        render json: {:aaData => @vendor_qualitis}
-      }
-    end
-  end
-
-  # GET /vendor_qualities/1
-  # GET /vendor_qualities/1.json
-  def show
-    @vendor_quality = VendorQuality.find(params[:id])
-    @attachable = @vendor_quality
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @vendor_quality }
-    end
-  end
-
-  # GET /vendor_qualities/new
-  # GET /vendor_qualities/new.json
-  def new
-    @duplicate = VendorQuality.find_by_id(params[:quality_id])
-    @vendor_quality = @duplicate.present? ? @duplicate.dup : VendorQuality.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @vendor_quality }
-    end
-  end
-
-  # GET /vendor_qualities/1/edit
-  def edit
-    @vendor_quality = VendorQuality.find(params[:id])
-  end
-
-  # POST /vendor_qualities
-  # POST /vendor_qualities.json
-  def create
-    @vendor_quality = VendorQuality.new(vendor_quality_params)
-
-    respond_to do |format|
-      if @vendor_quality.save
-        format.html { redirect_to vendor_qualities_url, notice: 'Quality ID was successfully created.' }
-        format.json { render json: @vendor_quality, status: :created, location: @vendor_quality }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @vendor_quality.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /vendor_qualities/1
-  # PUT /vendor_qualities/1.json
-  def update
-    @vendor_quality = VendorQuality.find(params[:id])
-
-    respond_to do |format|
-      if @vendor_quality.update_attributes(vendor_quality_params)
-        format.html { redirect_to vendor_qualities_url, notice: 'Quality ID was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @vendor_quality.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /vendor_qualities/1
-  # DELETE /vendor_qualities/1.json
-  def destroy
-    @vendor_quality = VendorQuality.find(params[:id])
-    @vendor_quality.destroy
-
-    respond_to do |format|
-      format.html { redirect_to vendor_qualities_url }
-      format.json { head :no_content }
-    end
-  end
-
-  def set_default
-    @vendor_quality = VendorQuality.find(params[:vendor_quality_id])
-    respond_to do |format|
-      if @vendor_quality
-          MasterType.where(:type_category => "default_vendor_quality").destroy_all
-          MasterType.create(:type_name => "Default Vendor Quality", :type_description => "", :type_value => @vendor_quality.id, :type_category => "default_vendor_quality", :type_active => true)
-      end
-      format.html { redirect_to @vendor_quality, notice: 'Quality ID was successfully added as default.' }
-      format.json { head :no_content }
-    end
-  end
   private
 
     def set_vendor_quality
--- app/models/vendor_quality.rb
+++ app/models/vendor_quality.rb
@@ -12,43 +12,27 @@
 #
 
 class VendorQuality < ActiveRecord::Base
-  include Rails.application.routes.url_helpers
+  has_many :organizations
+  has_many :max_quality_customers, :class_name => "Organization", :foreign_key => "customer_max_quality_id"
+  has_many :min_quality_customers, :class_name => "Organization", :foreign_key => "customer_min_quality_id"
+  has_many :item_revisions
+  has_many :po_lines
+  has_many :attachments, :as => :attachable, :dependent => :destroy
 
-  attr_accessible :quality_active, :quality_created_id, :quality_description, 
-  :quality_name, :quality_notes, :quality_updated_id
+  (validates_uniqueness_of :quality_name if validates_length_of :quality_name, :minimum => 1, :maximum => 1) if validates_presence_of :quality_name
+  validates_format_of :quality_name, :with => /^[a-zA-Z]*$/i, :multiline => true,:message => "can only contain letter!"
+  validates_length_of :quality_description, :maximum => 50
+  validates_length_of :quality_notes, :maximum => 200
 
-   after_initialize :default_values
+  before_validation :before_validate_values
+  after_initialize :default_values
 
   def default_values
     self.quality_active = true if self.quality_active.nil?
   end
 
-  before_validation :before_validate_values
-
   def before_validate_values
       self.quality_name = self.quality_name.upcase
-  end
-
-  (validates_uniqueness_of :quality_name if validates_length_of :quality_name, :minimum => 1, :maximum => 1) if validates_presence_of :quality_name
-
-  validates_format_of :quality_name, :with => /^[a-zA-Z]*$/i, :multiline => true,:message => "can only contain letter!"
-
-  validates_length_of :quality_description, :maximum => 50
-
-  validates_length_of :quality_notes, :maximum => 200
-
-  has_many :organizations
-
-  has_many :max_quality_customers, :class_name => "Organization", :foreign_key => "customer_max_quality_id"
-
-  has_many :min_quality_customers, :class_name => "Organization", :foreign_key => "customer_min_quality_id"
-
-  has_many :item_revisions
-  has_many :po_lines
-  has_many :attachments, :as => :attachable, :dependent => :destroy
-
-  def redirect_path
-      vendor_quality_path(self)
   end
 
   def default_quality
