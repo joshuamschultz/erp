db13eecf8fc2383e58215132d46b3c6ed350b55d
Joshua Schultz <joshuamschultz@gmail.com>
2017-12-22 15:37:34 -0500

    MODELS: cause analysis, capacity, attachment

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -14,13 +14,15 @@
 class Checklist < ActiveRecord::Base
   belongs_to :quality_lot
   belongs_to :po_line
-  belongs_to :customer_quality   
+  belongs_to :customer_quality
 
   has_many :check_list_lines, :dependent => :destroy
 
   validate :quality_lot, :po_line, :customer_quality
   validates_uniqueness_of :quality_lot_id
+
   def checklist_params
-      params.require(:checklist).permit(:checklist_status, :quality_lot_id, :po_line_id, :customer_quality_id)
-  end      
+    params.require(:checklist).permit(:checklist_status, :quality_lot_id, :po_line_id, :customer_quality_id)
+  end
+
 end
