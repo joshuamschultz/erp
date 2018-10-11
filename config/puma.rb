215719c634d92aa2cffe7846719b2cdf66077782
Joshua Schultz <joshuamschultz@gmail.com>
2018-10-10 21:43:21 -0400

    adjust puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,34 +1,24 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+#!/usr/bin/env puma
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+directory '/home/deploy/chess-erp'
+rackup "/home/deploy/chess-erp/current/config.ru"
+environment 'staging'
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+tag ''
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+pidfile "/home/deploy/chess-erp/shared/tmp/pids/puma.pid"
+state_path "/home/deploy/chess-erp/shared/tmp/pids/puma.state"
+stdout_redirect '/home/deploy/chess-erp/shared/log/puma_access.log', '/home/deploy/chess-erp/shared/log/puma_error.log', true
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+threads 0,16
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+bind 'unix:///home/deploy/chess-erp/shared/tmp/sockets/chess-erp-puma.sock'
+
+workers 0
+
+prune_bundler
+
+on_restart do
+  puts 'Refreshing Gemfile'
+  ENV["BUNDLE_GEMFILE"] = "/home/deploy/chess-erp/current/Gemfile"
+end
