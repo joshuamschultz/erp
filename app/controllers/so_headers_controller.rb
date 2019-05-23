484042ff4ea851d540c0385f645423df29b6da9a
Joshua Schultz <joshuamschultz@gmail.com>
2019-05-22 21:53:12 -0400

    clean up so_line view

--- app/controllers/so_headers_controller.rb
+++ app/controllers/so_headers_controller.rb
@@ -2,23 +2,9 @@ class SoHeadersController < ApplicationController
   before_action :find_relations, only: [:index]
   before_action :set_page_info
   before_action :set_autocomplete_values, only: [:create, :update]
-
-  before_action :view_permissions, except: [:index, :show]
-  before_action :user_permissions
+  before_action :set_so_header, only: %i[show edit update destroy, populate, so_info, report, pick_report, packing_report]
 
   autocomplete :so_header, :so_identifier, :full => true
-
-  def view_permissions
-    if user_signed_in? && (current_user.is_logistics? || current_user.is_quality? || current_user.is_customer?)
-      authorize! :edit, User
-    end
-  end
-
-  def user_permissions
-    if user_signed_in? && current_user.is_vendor?
-      authorize! :edit, User
-    end
-  end
 
   def set_page_info
     unless user_signed_in? && current_user.is_vendor?
@@ -59,97 +45,30 @@ class SoHeadersController < ApplicationController
   end
 
   def index
+    # TODO: deleting empty SO should be moved to a job at night - not every load
     if @so_headers.find_by_so_identifier("Unassigned").present?
       @so_headers.find_by_so_identifier("Unassigned").delete
       redirect_to action: "index"
+    elsif params[:so_status]
+      @so_headers = SoHeader.status_based_sos(params[:so_status])
     else
-      respond_to do |format|
-        format.html # index.html.erb
-        @so_heders = Array.new
-        format.json {
-          i = 0
-          po_type = MasterType.find_by_type_value("transer")
-          so_ids = PoHeader.where("po_type_id =? ", po_type.id).collect(&:so_header_id)
-          @so_headers = @so_headers.reject { |entry| so_ids.include? entry[:id] }
-          so_line_ids = []
-          @so_headers = @so_headers.select { |so_header|
-            so_heder = Hash.new
-            so_header.attributes.each do |key, value|
-              so_heder[key] = value
-            end
-            so_heder[:index] = i
-            so_heder[:so_id] = CommonActions.linkable(so_header_path(so_header), so_header.so_identifier)
-            so_heder[:customer_name] = CommonActions.linkable(organization_path(so_header.organization), so_header.organization.organization_name)
-            if params[:item_id].present?
-              so_lines = so_header.so_lines
-              if so_line_ids != nil?
-                so_lines = so_lines.reject { |entry| so_line_ids.include? entry[:id] }
-              end
-              so_line_ids << so_header.so_lines.first.id
-              so_heder[:so_line_price] = so_lines.first.so_line_sell
-              so_heder[:so_type_qty] = so_lines.first.so_line_quantity
-            end
-            if so_header.bill_to_address
-              so_heder[:bill_to_address_name] = CommonActions.linkable(contact_path(so_header.bill_to_address), so_header.bill_to_address.contact_description)
-            else
-              so_heder[:bill_to_address_name] = CommonActions.linkable(organization_main_address_path(so_header.organization), so_header.organization.organization_name)
-            end
-            if so_header.ship_to_address
-              so_heder[:ship_to_address_name] = CommonActions.linkable(contact_path(so_header.ship_to_address), so_header.ship_to_address.contact_description)
-            else
-              so_heder[:ship_to_address_name] = CommonActions.linkable(organization_main_address_path(so_header.organization), so_header.organization.organization_name)
-            end
-            if can? :edit, so_header
-              so_heder[:links] = CommonActions.object_crud_paths(nil, edit_so_header_path(so_header), nil)
-            else
-              so_heder[:links] = ""
-            end
-
-            i += 1
-            @so_heders.push(so_heder)
-          }
-          render json: { :aaData => @so_heders }
-        }
-      end
+      @so_headers = SoHeader.status_based_sos("open")
     end
   end
 
-  # GET /so_headers/1
-  # GET /so_headers/1.json
   def show
-    @so_header = SoHeader.find(params[:id])
-    if @so_header.so_identifier == "Unassigned"
-      @so_header.delete
-      redirect_to action: "index"
-    else
-      @attachable = @so_header
-      @notes = @so_header.present? ? @so_header.comments.where(:comment_type => "note").order("created_at desc") : []
-      @attachment = @so_header.attachments.new
-      respond_to do |format|
-        format.html # show.html.erb
-        format.json { render json: @so_header }
-      end
-    end
+    @attachable = @so_header
+    @notes = @so_header.present? ? @so_header.comments.where(:comment_type => "note").order("created_at desc") : []
+    @attachment = @so_header.attachments.new
   end
 
-  # GET /so_headers/new
-  # GET /so_headers/new.json
   def new
     @so_header = SoHeader.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @so_header }
-    end
   end
 
-  # GET /so_headers/1/edit
   def edit
-    @so_header = SoHeader.find(params[:id])
   end
 
-  # POST /so_headers
-  # POST /so_headers.json
   def create
     @so_header = SoHeader.new(so_header_params)
 
@@ -164,11 +83,7 @@ class SoHeadersController < ApplicationController
     end
   end
 
-  # PUT /so_headers/1
-  # PUT /so_headers/1.json
   def update
-    @so_header = SoHeader.find(params[:id])
-
     respond_to do |format|
       if @so_header.update_attributes(so_header_params)
         format.html { redirect_to new_so_header_so_line_path(@so_header), notice: "So header was successfully updated." }
@@ -180,21 +95,11 @@ class SoHeadersController < ApplicationController
     end
   end
 
-  # DELETE /so_headers/1
-  # DELETE /so_headers/1.json
   def destroy
-    @so_header = SoHeader.find(params[:id])
     @so_header.destroy
-
-    respond_to do |format|
-      format.html { redirect_to so_headers_url }
-      format.json { head :no_content }
-    end
   end
 
   def populate
-    @so_header = SoHeader.find(params[:id])
-
     if params[:type] == "note" && params[:comment].present?
       Comment.process_comments(current_user, @so_header, [params[:comment]], params[:type])
       note = @so_header.comments.where(:comment_type => "note").order("created_at desc").first if @so_header
@@ -210,7 +115,6 @@ class SoHeadersController < ApplicationController
   end
 
   def so_info
-    @so_header = SoHeader.find(params[:id])
     @organization = @so_header.organization if @so_header
     if @organization
       render :layout => false
@@ -220,18 +124,15 @@ class SoHeadersController < ApplicationController
   end
 
   def report
-    @so_header = SoHeader.find(params[:id])
     render :layout => false
   end
 
   def packing_report
-    @so_header = SoHeader.find(params[:id])
     @company_info = CompanyInfo.first
     render :layout => false
   end
 
   def pick_report
-    @so_header = SoHeader.find(params[:id])
     @company_info = CompanyInfo.first
     render :layout => false
   end
